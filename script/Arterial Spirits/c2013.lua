5b9f8338ffb696bf3cf1d5d47ab7a0d6fc47c340
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-20 00:51:51 +0700

    c2013.lua

--- script/Arterial Spirits/c2013.lua
+++ script/Arterial Spirits/c2013.lua
@@ -35,7 +35,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_ATTACK)
-	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	end
