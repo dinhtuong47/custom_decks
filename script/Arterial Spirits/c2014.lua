7922b302bc6ca52f595dfe4f77afe7574f3b7975
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-20 00:22:43 +0700

    Add files via upload

---
+++ script/Arterial Spirits/c2014.lua
@@ -0,0 +1,64 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x7D0}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsMainPhase()
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x7D0) and c:IsLevel(4)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	    Duel.BreakEffect()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+        --Unaffected
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_IMMUNE_EFFECT)
+		e2:SetTargetRange(LOCATION_ONFIELD,0)
+		e2:SetTarget(s.etg)
+		e2:SetValue(s.efilter)
+		e2:SetLabelObject(re)
+		e2:SetReset(RESET_CHAIN)
+		Duel.RegisterEffect(e2,tp)
+		end
+		Duel.BreakEffect()
+		if tc:IsCode(2000) and tc:IsRelateToEffect(e) then
+		Duel.BreakEffect()
+		--end main phase
+		Duel.SkipPhase(1-tp,Duel.GetCurrentPhase(),RESET_PHASE+PHASE_END,1)
+	end
+end
+function s.etg(e,c)
+	return c:IsFaceup() or c:IsFacedown()
+end
+function s.efilter(e,re)
+	return re==e:GetLabelObject()
+end
+
+ 
