9ff8e7f715bb8775a72e387e9b0088e364e45319
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-20 23:41:48 +0700

    c117.lua

--- script/Shaco/c117.lua
+++ script/Shaco/c117.lua
@@ -1,22 +1,57 @@
 local s,id=GetID()
 function s.initial_effect(c)
+	--destroy
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetCategory(CATEGORY_DESTROY)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_SUMMON_SUCCESS)
+	e0:SetCondition(s.spcon)
+	e0:SetTarget(s.destg)
+	e0:SetOperation(s.desop)
+	c:RegisterEffect(e0)
+	local e1=e0:Clone()
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e1)
 	--neg attack
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_ACTIVATE)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e2:SetCondition(s.cbcon)
 	e2:SetOperation(s.cbop)
 	c:RegisterEffect(e2)
-	--neg
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_NEGATE)
-	e4:SetType(EFFECT_TYPE_ACTIVATE)
-	e4:SetCode(EVENT_CHAINING)
-	e4:SetCondition(s.discon)
-	e4:SetOperation(s.disop)
-	c:RegisterEffect(e4)	
+	--neg effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_NEGATE)
+	e3:SetType(EFFECT_TYPE_ACTIVATE)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetCondition(s.discon)
+	e3:SetOperation(s.disop)
+	c:RegisterEffect(e3)	
+end
+--des
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp) 
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,111),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.desfilter(c,g)
+	return g:IsContains(c)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local cg=e:GetHandler():GetColumnGroup()
+	if chkc then return chkc:IsOnField() and s.filter(chkc,cg) end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,cg) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,cg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
 end
 --neg attack
 function s.cbcon(e,tp,eg,ep,ev,re,r,rp)
@@ -26,7 +61,7 @@ end
 function s.cbop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateAttack()
 end
---neg
+--neg effect
 function s.cfilter(c,seq,p)
 	return c:IsFaceup() and c:IsCode(111) and c:IsColumn(seq,p,LOCATION_MZONE)
 end
