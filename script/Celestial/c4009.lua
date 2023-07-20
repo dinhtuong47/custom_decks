f0fc568c0b59adbd0b75fb4e1a41550790f6c920
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-20 20:32:36 +0700

    c4009.lua

--- script/Celestial/c4009.lua
+++ script/Celestial/c4009.lua
@@ -6,58 +6,27 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetOperation(s.activate)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-	--place on top
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TODECK)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetCode(EVENT_TO_DECK)
-	e2:SetRange(LOCATION_FZONE)
-	e2:SetCountLimit(1)
-	e2:SetCost(s.cost)
-	e2:SetTarget(s.tdtg)
-	e2:SetOperation(s.tdop)
-	c:RegisterEffect(e2)
 end
-s.listed_series={0xFA0}
-function s.filter(c,e,tp)
-	return c:IsSetCard(0xFA0) and c:IsMonster() and c:IsAbleToHand()
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>2 end
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp) -- Add to hand
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+function s.thfilter(c)
+	return c:IsLevel(6) and c:IsAbleToHand()
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
+	Duel.ConfirmDeckbottom(tp,3)
+	local g=Duel.GetDeckbottomGroup(tp,3)
+	if g:IsExists(s.thfilter,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local sg=g:Select(tp,1,1,nil)
+		local sg=g:FilterSelect(tp,s.thfilter,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg)
+		Duel.ShuffleHand(tp)
 	end
+	Duel.ShuffleDeck(tp)
 end
---place on top
-function s.costfilter(c)
-	return c:IsCode(4003) and c:IsDiscardable()
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
-	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
-	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD)
-end
-function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_DECK,0,nil,0xFA0)
-		return aux.SelectUnselectGroup(g,e,tp,1,3,nil,chk)
-	end
-end
-function s.tdop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_DECK,0,nil,0xFA0)
-	local rg=aux.SelectUnselectGroup(g,e,tp,1,3,nil,1,tp,aux.Stringid(id,1))
-	if #rg>0 then
-		Duel.ConfirmCards(1-tp,rg)
-		Duel.ShuffleDeck(tp)
-		Duel.MoveToDeckTop(rg)
-		Duel.SortDecktop(tp,tp,#rg)
-	end
-end
+
