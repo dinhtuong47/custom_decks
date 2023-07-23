9d9c66efed063683f75a2d737bf44713cc661563
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-24 00:15:12 +0700

    c4003.lua

--- script/Celestial/c4003.lua
+++ script/Celestial/c4003.lua
@@ -32,11 +32,12 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if #tc>0 then Duel.SendtoHand(tc,nil,REASON_EFFECT) 
-	    Duel.ConfirmCards(1-tp,tc)        
+	if #tc>0 then Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	    Duel.ConfirmCards(1-tp,tc) end       
+		if Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-		if #g>0 then Duel.SelectYesNo(tp,aux.Stringid(id,2)) 
+		if #g>0 then
 			Duel.HintSelection(g,true)
 			Duel.BreakEffect()
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
