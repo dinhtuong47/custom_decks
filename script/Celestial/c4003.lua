a69cb5aeface27d90676709f83843b8c664a64e7
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-23 23:59:42 +0700

    c4003.lua

--- script/Celestial/c4003.lua
+++ script/Celestial/c4003.lua
@@ -12,20 +12,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-	--draw
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DRAW)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_DRAW)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,id+50)
-	e2:SetCondition(s.drcon)
-	e2:SetCost(s.drcost)
-	e2:SetTarget(s.drtg)
-	e2:SetOperation(s.drop)
-	c:RegisterEffect(e2)
+	--place
 end
 --add
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -33,39 +20,27 @@ function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 end
 function s.thfilter(c,tp)
-	return c:IsSetCard(0xFA1) and c:GetType()==TYPE_SPELL and c:IsAbleToHand() 
-end
-function s.tdfilter(c,deckCount)
-	return c:IsSetCard(0xFA0) or c:IsSetCard(0xFA1) and (c:IsLocation(LOCATION_DECK) 
-	and deckCount>1 or not c:IsLocation(LOCATION_DECK) and c:IsAbleToDeck())
+	return c:IsCode(67378104) and c:IsAbleToHand() 
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
-	and Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if #tc>0 then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tc)
-		local ct=Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)
-	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.tdfilter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,ct):GetFirst()
-	if tc then
-		if tc:IsLocation(LOCATION_DECK) then
-			Duel.ShuffleDeck(tp)
-			Duel.MoveToDeckTop(tc)
-		else 
-			Duel.HintSelection(tc,true)
-			Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)
-		end
-		if not tc:IsLocation(LOCATION_EXTRA) then
-			Duel.ConfirmDecktop(tp,1)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			Duel.BreakEffect()
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
 		end
 	end
 	local e1=Effect.CreateEffect(e:GetHandler())
@@ -78,41 +53,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e1,tp)
 	end
 end
---draw
-function s.filter(c)
-	return c:IsLevel(6) and not c:IsPublic()
-end
-function s.drcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp and Duel.GetTurnPlayer()==tp  
-end
-
-function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToDeck() and eg:IsExists(s.filter,1,nil) end
-	local g=eg:Filter(s.filter,nil)
-	Duel.SendtoDeck(c,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
-	if #g==1 then
-	    Duel.SendtoDeck(c,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-		Duel.ShuffleHand(tp)
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-		local sg=g:Select(tp,1,1,nil)
-		Duel.ConfirmCards(1-tp,sg)
-		Duel.ShuffleHand(tp)
-	end
-end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
-end
-
+--place
 
 
 		
