f750bf281f29ea3e39b7f26031c7b2af5e123093
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-26 13:38:10 +0700

    c4004.lua

--- script/Celestial/c4004.lua
+++ script/Celestial/c4004.lua
@@ -63,7 +63,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(1200)
+			e1:SetValue(600)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			tc2:RegisterEffect(e1)	
 		end
