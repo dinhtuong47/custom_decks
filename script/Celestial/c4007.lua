b5303eac443acc73479e479fbe580413bb1e7b51
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-26 23:46:03 +0700

    c4007.lua

--- script/Celestial/c4007.lua
+++ script/Celestial/c4007.lua
@@ -1,3 +1,4 @@
+--lord
 local s,id=GetID()
 function s.initial_effect(c)
 	--fusion summon
@@ -107,6 +108,30 @@ function s.activate2(e,tp,eg,ep,ev,re,r,rp)
 	    	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	    	tc:RegisterEffect(e1)
 	     	end
+			--Can only attack with 1 monster this turn
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetCondition(function(e) return e:GetLabel()~=0 end)
+	e1:SetTarget(function(e,c) return c:GetFieldID()~=e:GetLabel() end)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetLabelObject(e1)
+	e2:SetOperation(s.checkop)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+	aux.RegisterClientHint(c,0,tp,1,0,aux.Stringid(id,2))
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local fid=eg:GetFirst():GetFieldID()
+	e:GetLabelObject():SetLabel(fid)
+                        end
 		end
 	end
 end
