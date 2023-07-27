a1d621d9af1bf1dc3115a1ffb28bd5824a89d3ec
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-27 18:38:59 +0700

    c4009.lua

--- script/Celestial/c4009.lua
+++ script/Celestial/c4009.lua
@@ -49,9 +49,9 @@ function s.repfilter(c,tp)
 		and c:IsControler(tp) and c:IsReason(REASON_EFFECT+REASON_BATTLE) and not c:IsReason(REASON_REPLACE)
 end
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 and e:GetHandler():IsAbleToDeck() and eg:IsExists(s.repfilter,1,nil,tp) end
+	if chk==0 then return e:GetHandler():IsAbleToDeck() and not e:GetHandler():IsStatus(STATUS_DESTROY_CONFIRMED)
+		and eg:IsExists(s.repfilter,1,nil,tp) end
 	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
-		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
 		return true
 	else
 		return false
