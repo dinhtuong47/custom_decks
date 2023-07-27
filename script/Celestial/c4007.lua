71a1615844a11bbe70022f16d6d166e4949af34d
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-27 17:06:10 +0700

    c4007.lua

--- script/Celestial/c4007.lua
+++ script/Celestial/c4007.lua
@@ -4,21 +4,21 @@ function s.initial_effect(c)
 	--fusion summon
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,4001,s.matfilter)  
-	--control
+	--send bottom card
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_CONTROL)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetHintTiming(0,TIMING_BATTLE_END)
 	e1:SetCondition(s.ctcon)
-	e1:SetCountLimit(1,0)
+	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--add
+	--add returned card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND)
@@ -26,7 +26,7 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_DECK)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,0)
+	e2:SetCountLimit(1,id+50)
 	e2:SetCondition(s.gspcon)
 	e2:SetTarget(s.target2)
 	e2:SetOperation(s.activate2)
@@ -36,41 +36,28 @@ s.listed_names={4001}
 function s.matfilter(c,fc,sumtype,tp)
 	return c:GetLevel()==6
 end
---control
+--gain atk
 function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsBattlePhase() 
 end
-function s.cfilter(c)
-	return c:IsFaceup() and c:HasLevel() and c:GetLevel()>0 and c:IsControlerCanBeChanged()
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and s.cfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
-	local g=Duel.SelectTarget(tp,s.cfilter,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local sc=Duel.GetMatchingGroup(Card.IsSequence,tp,LOCATION_DECK,0,nil,0):GetFirst()
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=1 and sc and sc:IsAbleToGrave() end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and Duel.GetControl(tc,tp,PHASE_END,1) then
-		local c=e:GetHandler()
+	local sc=Duel.GetMatchingGroup(Card.IsSequence,tp,LOCATION_DECK,0,nil,0):GetFirst()
+	if sc:IsAbleGrave() and Duel.SendtoGrave(sc,nil,REASON_EFFECT)>0 then
+	local c=e:GetHandler()
+	local tc=Duel.GetOperatedGroup():GetFirst()
+	if tc and c:IsRelateToEffect(e) and c:IsFaceup() and tc:IsMonster() and tc:IsLevel(6) and tc:IsLocation(LOCATION_GRAVE) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e2)
-		local e3=Effect.CreateEffect(c)
-		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetCode(EFFECT_CHANGE_LEVEL)
-		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e3:SetValue(6)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e3)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(tc:GetAttack())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		end
 	end
 end
 --add
