70b1690bf3380040a61d89bdda8e4a1c72bbe232
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-07-31 08:21:47 +0700

    Add files via upload

--- script/Celestial/c4008.lua
+++ script/Celestial/c4008.lua
@@ -1,106 +1,146 @@
---God
 local s,id=GetID()
 function s.initial_effect(c)
-	--fusion material
+	--fusion summon
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,4000,4002,4003)
-	--place
+	--Special summon condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.FALSE)
+	c:RegisterEffect(e0)
+	--control
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TODECK)
-	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e1:SetCategory(CATEGORY_CONTROL)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetHintTiming(TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END+TIMING_END_PHASE)
+	e1:SetCondition(s.ctcon)
 	e1:SetCountLimit(1,id)
-	e1:SetCost(s.remcost)
-	e1:SetTarget(s.remtg)
-	e1:SetOperation(s.remop)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--add
+	--add returned card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_DECK)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id+50)
-	e2:SetCondition(s.addcon)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
+	e2:SetCondition(s.gspcon)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.activate2)
 	c:RegisterEffect(e2)
-	--[[spsummon condition
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e3:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e3:SetValue(aux.fuslimit)
-	c:RegisterEffect(e3)]]--
 end
-s.listed_names={4000,4002,4003}
---place
-function s.remcost(e,tp,eg,ep,ev,re,r,rp,chk)
+--control
+function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase() 
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:HasLevel() and c:GetLevel()>0 and c:IsControlerCanBeChanged()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and s.cfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,s.cfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.GetControl(tc,tp,PHASE_END,1) then
+		local c=e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_CHANGE_LEVEL)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetValue(6)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e3)
+	end
+end
+--add the returned card
+function s.gspconfilter(c,tp)
+	return  c:IsType(TYPE_MONSTER) and c:IsLocation(LOCATION_DECK) and not c:IsLocation(LOCATION_EXTRA)
+end
+function s.gspconfilter2(c,e,tp)
+	return s.gspconfilter(c,tp) and c:IsAbleToHand() 
+end
+function s.gspcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.gspconfilter,1,nil)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
+	local g=eg:Filter(s.gspconfilter2,nil,1-tp)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
 end
-function s.remtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		if Duel.IsPlayerAffectedByEffect(1-tp,69832741) then
-			return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,0x0e,1,nil)
-		else
-			return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,0x1e,1,nil)
-		end
-	end
-	if Duel.IsPlayerAffectedByEffect(1-tp,69832741) then
-		Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,0,0x0e)
-	else
-		Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,0,0x1e)
-	end
-end
-function s.rmfilter(c)
-	return c:IsAbleToDeck() and aux.SpElimFilter(c)
-end
-function s.remop(e,tp,eg,ep,ev,re,r,rp)
-	local g2=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,nil)
-	local g3=Duel.GetMatchingGroup(s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,nil)
-	local g1=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_HAND,nil)
-	local sg=Group.CreateGroup()
-	if #g1>0 and ((#g2==0 and #g3==0) or Duel.SelectYesNo(tp,aux.Stringid(id,0))) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-		local sg1=g1:RandomSelect(tp,1)
-		sg:Merge(sg1)
-	end
-	if #g2>0 and ((#sg==0 and #g3==0) or Duel.SelectYesNo(tp,aux.Stringid(id,1))) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-		local sg2=g2:Select(tp,1,1,nil)
-		Duel.HintSelection(sg2)
-		sg:Merge(sg2)
-	end
-	if #g3>0 and (#sg==0 or Duel.SelectYesNo(tp,aux.Stringid(id,2))) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-		local sg3=g3:Select(tp,1,1,nil)
-		Duel.HintSelection(sg3)
-		sg:Merge(sg3)
-	end
-	Duel.SendtoDeck(sg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
-end
---add
-function s.addcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return rp==1-tp and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) 
-end
-function s.thfilter(c,tp)
-	return c:IsLevel(6) and c:IsAbleToHand() 
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+	local g=Duel.GetTargetCards(e)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+	local rg=g:Select(tp,1,1,nil)
+	if #rg>0 then
+		Duel.SendtoHand(rg,tp,REASON_EFFECT)
+		if rg:IsExists(Card.IsLocation,1,nil,LOCATION_HAND) then
+		Duel.ConfirmCards(1-tp,rg)
+		local tc=rg:GetFirst()
+		for tc in aux.Next(rg) do
+		local e1=Effect.CreateEffect(c)
+	    	e1:SetType(EFFECT_TYPE_SINGLE)
+	    	e1:SetCode(EFFECT_CHANGE_LEVEL)
+	    	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	    	e1:SetValue(6)
+	    	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	    	tc:RegisterEffect(e1)
+	     	end
 	end
 end
+--Can only attack with 1 monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
+	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetCondition(s.limitcon)
+	e2:SetTarget(s.limittg)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetOperation(s.checkop)
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	e3:SetLabelObject(e2)
+	Duel.RegisterEffect(e3,tp)
+end
+function s.limitcon(e)
+	return e:GetLabel()~=0
+end
+function s.limittg(e,c)
+	return c:GetFieldID()~=e:GetLabel()
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local fid=eg:GetFirst():GetFieldID()
+	e:GetLabelObject():SetLabel(fid)
+end
+
+
+
