ad6d1ad899fcca3ffce2a043e3f59330e8b69a75
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-01 22:45:12 +0700

    c4008.lua

--- script/Celestial/c4008.lua
+++ script/Celestial/c4008.lua
@@ -26,7 +26,7 @@ function s.initial_effect(c)
 	--add returned card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_DECK)
