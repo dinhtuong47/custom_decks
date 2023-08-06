248ff9df720526b569c54e428d537c1c61e0185c
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-06 23:20:17 +0700

    Update c3014.lua

--- script/Solo Leveling/c3014.lua
+++ script/Solo Leveling/c3014.lua
@@ -3,7 +3,7 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_TOGRAVE)
+	e1:SetCategory(CATEGORY_NEGATE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetTarget(s.target)
@@ -11,9 +11,13 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	local cd=re:GetHandler():GetType()
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil,cd)
+	if chk==0 then return g:GetClassCount(Card.GetType)>=2 end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,tp,LOCATION_HAND)
+end
+function s.cfilter(c)
+	return c:IsSetCard(0xBB8) and not c:IsPublic()
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) then
@@ -21,8 +25,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			Duel.SendtoGrave(eg,REASON_EFFECT)
 		end
 		local cd=re:GetHandler():GetType()
-		local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND,0,nil,cd)
-		if #g>0 then
+		local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil,cd)
+		if #g>1 then
 			Duel.ConfirmCards(1-tp,g,REASON_EFFECT)
 		end
 	end
