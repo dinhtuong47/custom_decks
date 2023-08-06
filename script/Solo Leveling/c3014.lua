2aec74f234410fad50b157e544567af0e24c0964
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 00:09:08 +0700

    Update c3014.lua

--- script/Solo Leveling/c3014.lua
+++ script/Solo Leveling/c3014.lua
@@ -2,27 +2,54 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_NEGATE)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCondition(s.condition)
 	e1:SetCode(EVENT_CHAINING)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.negcon)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
-end
-function s.cfilter(c)
-	return c:IsSetCard(0xBB8) and not c:IsPublic()  
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+		and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(s.cfilter,ep,LOCATION_HAND,0,nil,re:GetHandler():GetType())
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
+function s.cfilter(c)
+	return c:IsSetCard(0xBB8) and not c:IsPublic()
+end	
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.cfilter,ep,LOCATION_HAND,0,nil,re:GetHandler():GetType())
-	if #g<1 then return end
-			Duel.ConfirmCards(1-tp,g,REASON_EFFECT)
+	local ec=re:GetHandler()
+	Duel.NegateActivation(ev) end
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil)
+	if #g>1 and g:GetClassCount(Card.GetCode)>=2 and ec:IsRelateToEffect(re)
+		    and ec:IsCanTurnSet() and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_CONFIRM)
+	Duel.ConfirmCards(1-tp,sg)
+		ec:CancelToGrave()
+		Duel.ChangePosition(ec,POS_FACEDOWN)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetTargetRange(0,1)
+		e1:SetLabel(ec:GetOriginalCode())
+		e1:SetValue(s.aclimit)
+		e1:SetReset(RESET_PHASE+PHASE_END,2)
+		Duel.RegisterEffect(e1,tp)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e2:SetDescription(aux.Stringid(id,1))
+		e2:SetReset(RESET_PHASE+PHASE_END,2)
+		e2:SetTargetRange(0,1)
+		Duel.RegisterEffect(e2,tp)
+	end
 end
+function s.aclimit(e,re,tp)
+	return re:GetHandler():GetOriginalCode()==e:GetLabel()
+end
+
