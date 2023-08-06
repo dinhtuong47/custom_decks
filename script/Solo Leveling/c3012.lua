bbf06d1ed79680376b4689bb5c2cef80331ccc03
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-06 21:29:39 +0700

    Add files via upload

--- script/Solo Leveling/c3012.lua
+++ script/Solo Leveling/c3012.lua
@@ -2,7 +2,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Set
 	local e0=Effect.CreateEffect(c)
-	e0:SetDescription(aux.Stringid(id,1))
+	e0:SetDescription(aux.Stringid(id,0))
 	e0:SetCategory(CATEGORY_SUMMON)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e0)
 	--Flip
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -20,8 +20,13 @@ function s.initial_effect(c)
 	e1:SetTarget(s.postg)
 	e1:SetOperation(s.posop)
 	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetTarget(s.postg2)
+	e2:SetOperation(s.posop2)
+	c:RegisterEffect(e2)
 end
---set
+--set from hand
 function s.nsfilter(c)
 	return c:IsSetCard(0xBB8) and c:IsSummonable(true,nil)
 end
@@ -36,9 +41,9 @@ function s.nsop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.MSet(tp,sc,true,nil)
 	end
 end
---flip
+--set from field
 function s.posfilter(c)
-	return c:IsSetCard(0xBB8) and c:IsFacedown() and c:IsCanChangePosition()
+	return c:IsSetCard(0xBB8) and c:IsFaceup() and c:IsCanChangePosition()
 end
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,nil) end
@@ -47,6 +52,21 @@ end
 function s.posop(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
 	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g>0 then
+		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
+	end
+end
+--flip
+function s.posfilter2(c)
+	return c:IsSetCard(0xBB8) and c:IsFacedown() and c:IsCanChangePosition()
+end
+function s.postg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter2,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.posop2(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter2,tp,LOCATION_MZONE,0,1,1,nil)
 	if #g>0 then
 		Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
 	end
