7d239857f56f897a1e2c6ee34c22d5323ad4db69
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 00:57:03 +0700

    Update c3010.lua

--- script/Solo Leveling/c3010.lua
+++ script/Solo Leveling/c3010.lua
@@ -32,7 +32,6 @@ end
 function s.cfilter(c)
 	return c:IsSetCard(0xBB8) and not c:IsPublic()
 end
-local key=TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP
 function s.rescon(sg,e,tp,mg)
 	return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,sg)
 end
