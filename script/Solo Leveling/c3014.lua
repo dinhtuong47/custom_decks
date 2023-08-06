aa3665230927b2d3cbddd4f03b9046668ea68677
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-06 23:36:44 +0700

    c3014.lua

--- script/Solo Leveling/c3014.lua
+++ script/Solo Leveling/c3014.lua
@@ -15,7 +15,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
 end
 function s.cfilter(c)
-	return c:IsSetCard(0xBB8) and not c:IsPublic() 
+	return c:IsSetCard(0xBB8) and not c:IsPublic() and not sg:IsExists(Card.IsType,1,nil,c:GetType())
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
