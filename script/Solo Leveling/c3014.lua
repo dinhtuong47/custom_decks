7dc084ea82237d00c0f2924a9f4dd1eb45733643
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 00:23:12 +0700

    Update c3014.lua

--- script/Solo Leveling/c3014.lua
+++ script/Solo Leveling/c3014.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.cdfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xBB8)
+	return c:IsFaceup() and c:IsType(TYPE_FLIP)
 end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(s.cdfilter,tp,LOCATION_MZONE,0,1,nil)
