f004137377ef73224e26561af426248372d15167
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 10:41:32 +0700

    Update c3000.lua

--- script/Solo Leveling/c3000.lua
+++ script/Solo Leveling/c3000.lua
@@ -65,7 +65,7 @@ function s.posfilter(c)
 	return c:IsMonster()
 end
 function s.poscon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp 
+	return Duel.GetTurnPlayer()~=tp and Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)>=2
 end
 function s.poscost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToHandAsCost() end
