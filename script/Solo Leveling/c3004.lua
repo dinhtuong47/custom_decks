89c473ec268670ec88bc68cdb12805c23cc77e36
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 14:01:38 +0700

    Update c3004.lua

--- script/Solo Leveling/c3004.lua
+++ script/Solo Leveling/c3004.lua
@@ -1,11 +1,6 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	--immu to deck
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e0:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	e0:SetOperation(s.imuop)
-	c:RegisterEffect(e0)
+	
 	--flip effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
@@ -36,16 +31,7 @@ function s.initial_effect(c)
 	e3:SetOperation(s.setop)
 	c:RegisterEffect(e3)
 end
-function s.imuop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetCode(EFFECT_CANNOT_TO_DECK)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(1)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-	e:GetHandler():RegisterEffect(e1)
-end
+
 --add
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|(RESETS_STANDARD|RESET_OVERLAY)&~(RESET_LEAVE|RESET_TOGRAVE),0,0)
