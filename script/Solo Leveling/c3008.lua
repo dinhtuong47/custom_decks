8de7fd58fc8fb644f057b1759a084c3fb7549ed0
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 10:27:09 +0700

    Update c3008.lua

--- script/Solo Leveling/c3008.lua
+++ script/Solo Leveling/c3008.lua
@@ -17,4 +17,50 @@ function s.initial_effect(c)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_DECREASE_TRIBUTE)
 	c:RegisterEffect(e3)
+	--Prevent effect target
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTarget(function(e,c) return c:IsFacedown() end)
+	e3:SetValue(aux.indoval)
+	c:RegisterEffect(e3)
+	--ss from gy
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetCode(EVENT_BATTLE_DESTROYED)
+	e4:SetCountLimit(1,id)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCondition(s.spcon)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	local rc=tc:GetReasonCard()
+	return #eg==1 and rc:IsControler(tp) and rc:IsSetCard(0xBB8)
+		and tc:IsMonster() and tc:IsReason(REASON_BATTLE) and tc:IsLocation(LOCATION_GRAVE)
+	        and tc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=eg:GetFirst()
+	local rc=tc:GetReasonCard()
+	local bc=rc:GetBattleTarget()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and bc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE) end
+	Duel.SetTargetCard(bc)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,bc,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+		Duel.ConfirmCards(1-tp,tc)
+		end
+	end
 end
