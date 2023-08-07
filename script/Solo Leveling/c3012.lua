f134411df8df8038d4378609b042e64d64361adb
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 10:27:54 +0700

    Update c3012.lua

--- script/Solo Leveling/c3012.lua
+++ script/Solo Leveling/c3012.lua
@@ -9,7 +9,23 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--Flip & set from field
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e2:SetTarget(s.postg)
+	e2:SetOperation(s.posop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetTarget(s.postg2)
+	e3:SetOperation(s.posop2)
+	c:RegisterEffect(e3)
 end
+--set from hand
 function s.filter(c)
 	return c:IsSummonable(true,nil)
 end
@@ -25,3 +41,34 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.MSet(tp,tc,true,nil)
 	end
 end
+--set from field
+function s.posfilter(c)
+	return c:IsSetCard(0xBB8) and c:IsFaceup() and c:IsCanChangePosition()
+end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g>0 then
+		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
+	end
+end
+--flip
+function s.posfilter2(c)
+	return c:IsSetCard(0xBB8) and c:IsFacedown() and c:IsCanChangePosition()
+end
+function s.postg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter2,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.posop2(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter2,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g>0 then
+		Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
+	end
+end
+
