b49cb73c94ca581d8b2251ae1f6b9fcfebc5fec1
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 20:29:24 +0700

    Update c3001.lua

--- script/Solo Leveling/c3001.lua
+++ script/Solo Leveling/c3001.lua
@@ -73,23 +73,26 @@ function s.poscost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToHandAsCost() end
 	Duel.SendtoHand(e:GetHandler(),nil,REASON_COST)
 end
-function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
-end
-function s.posop(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local tc=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
-	local opt=0
-	if tc:IsPosition(POS_FACEDOWN) then
-		opt=POS_FACEUP_DEFENSE
-	elseif tc:IsPosition(POS_FACEUP_ATTACK) then
-		opt=POS_FACEUP_DEFENSE+POS_FACEDOWN_DEFENSE
-	elseif tc:IsPosition(POS_FACEUP_DEFENSE) then
-		opt=POS_FACEDOWN_DEFENSE
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		if tc:IsAttackPos() then
+			local pos=0
+			if tc:IsCanTurnSet() then
+				pos=Duel.SelectPosition(tp,tc,POS_DEFENSE)
+			else
+				pos=Duel.SelectPosition(tp,tc,POS_FACEUP_DEFENSE)
+			end
+			Duel.ChangePosition(tc,pos)
+		else
+			Duel.ChangePosition(tc,0,0,POS_FACEDOWN_DEFENSE,POS_FACEUP_DEFENSE)
+		end
 	end
-	if opt==0 then return end
-	local pos=Duel.SelectPosition(tp,tc,opt)
-	if pos==0 then return end
-	Duel.ChangePosition(tc,pos)
 end
