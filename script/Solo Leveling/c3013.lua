69e92717508bfea50c8c0c097a8d8be326d69a8f
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 11:28:44 +0700

    Update c3013.lua

--- script/Solo Leveling/c3013.lua
+++ script/Solo Leveling/c3013.lua
@@ -37,7 +37,7 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	if not tc or not tc:IsRelateToEffect(e) then return end
 	local andifyoudo=false
 	if tc:IsMonster() then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)>0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)>0 then
 		Duel.ConfirmCards(1-tp,tc)
 		--Cannot be targeted for attacks
 			local e1=Effect.CreateEffect(e:GetHandler())
@@ -50,8 +50,6 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 			tc:RegisterEffect(e1)
 		end
 	end
-	Duel.SpecialSummonComplete()
-end
 	elseif tc:IsSpellTrap() then
 		if tc:IsType(TYPE_FIELD) then
 			local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
