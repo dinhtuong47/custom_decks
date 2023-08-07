987a5d80064a08b16e7350cd7db71d223834f931
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-07 10:28:45 +0700

    Update c3008.lua

--- script/Solo Leveling/c3008.lua
+++ script/Solo Leveling/c3008.lua
@@ -61,6 +61,5 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
 		Duel.ConfirmCards(1-tp,tc)
-		end
 	end
 end
