7698027c25e4e7b9730df3807b6eed9b6c9653ee
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-08 15:48:40 +0700

    Update c3005.lua

--- script/Solo Leveling/c3005.lua
+++ script/Solo Leveling/c3005.lua
@@ -80,5 +80,5 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
 	if #g~=0 and Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)>0 then 
 		Duel.BreakEffect()
-	        Duel.Damage(1-tp,800,REASON_EFFECT) 
+	        Duel.Damage(1-tp,800,REASON_EFFECT) end
 end
