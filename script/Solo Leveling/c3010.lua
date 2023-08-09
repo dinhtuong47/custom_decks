dc074eaaba90430ac22698480c86a75af4fa55c1
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-09 11:48:36 +0700

    Update c3010.lua

--- script/Solo Leveling/c3010.lua
+++ script/Solo Leveling/c3010.lua
@@ -35,22 +35,27 @@ local key=TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP
 function s.cfilter(c,tp)
 	return c:IsSetCard(0xBB8) and not c:IsPublic()  
 end
+function s.cfilter(c,tp)
+	return c:IsSetCard(0xBB8) and not c:IsPublic()  
+end
 function s.thfilter(c,ctype)
 	return c:IsSetCard(0xBB8) and c:IsAbleToHand() and not c:IsType(ctype&key)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil)
+	local g1=Duel.GetMatchingGroup(s.cfilter1,tp,LOCATION_HAND,0,nil)
+	local g1=Duel.GetMatchingGroup(s.cfilter2,tp,LOCATION_HAND,0,nil)
 	if chk==0 then return g:GetClassCount(Card.GetCode)>=2 end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil)
-	if #g<2 then return end
-	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_CONFIRM)
-	Duel.ConfirmCards(1-tp,sg)
+	local g1=Duel.GetMatchingGroup(s.cfilter1,tp,LOCATION_HAND,0,nil)
+	local g2=Duel.GetMatchingGroup(s.cfilter2,tp,LOCATION_HAND,0,nil)
+	if #g1>0 and #g2>0 then
+	Duel.ConfirmCards(1-tp,g1)
+	Duel.ConfirmCards(1-tp,g2)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local eq=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,sg:GetFirst():GetType())
+		local eq=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,g1:GetType(),g2:GetFirst():GetType())
 		if not eq then return end
 		Duel.BreakEffect()
 		Duel.SendtoHand(eq,tp,REASON_EFFECT)
