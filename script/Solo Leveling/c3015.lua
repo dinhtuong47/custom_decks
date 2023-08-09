114f7f793100cc6c18012011488fe7923c7c508e
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-09 20:46:31 +0700

    Update c3015.lua

--- script/Solo Leveling/c3015.lua
+++ script/Solo Leveling/c3015.lua
@@ -16,25 +16,17 @@ function s.initial_effect(c)
 	e4:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
 	e4:SetValue(2)
 	c:RegisterEffect(e4)
-	--flip effect
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_FLIP)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetOperation(s.flipop)
-	c:RegisterEffect(e1)
-	--add
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.retcon)
-	e2:SetTarget(s.rettg)
-	e2:SetOperation(s.retop)
-	c:RegisterEffect(e2)
+	--Add itself to the hand
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,1))
+	e6:SetCategory(CATEGORY_TOHAND)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e6:SetProperty(EFFECT_FLAG_DELAY)
+	e6:SetCode(EVENT_TO_GRAVE)
+	e6:SetCondition(s.thcon)
+	e6:SetTarget(s.thtg)
+	e6:SetOperation(s.thop)
+	c:RegisterEffect(e6)
 end	
 s.listed_series={0xbb8}
 function s.efilter(c)
@@ -44,24 +36,18 @@ function s.dircon(e)
 	return e:GetHandler():GetEquipTarget():GetAttackAnnouncedCount()>0
 end
 --add
-function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|(RESETS_STANDARD|RESET_OVERLAY)&~(RESET_LEAVE|RESET_TOGRAVE),0,0)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_LOST_TARGET) and c:GetPreviousEquipTarget() and c:GetPreviousEquipTarget():IsLocation(LOCATION_HAND)
 end
-function s.retcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(id)~=0
-end
-function s.filter(c,e,tp)
-	return c:GetCode()==id	and c:IsAbleToHand()
-end
-function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
-function s.retop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SendtoHand(c,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,c)
 	end
 end
 
