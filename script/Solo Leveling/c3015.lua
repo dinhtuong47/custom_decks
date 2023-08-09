6589d8e5e92e36960c5946cb06f4f9008ecd6527
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-09 20:41:34 +0700

    Update c3015.lua

--- script/Solo Leveling/c3015.lua
+++ script/Solo Leveling/c3015.lua
@@ -1,6 +1,6 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddEquipProcedure(c,nil,s.filter)
+	aux.AddEquipProcedure(c,nil,s.efilter)
 	--atk/def up
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
@@ -16,11 +16,52 @@ function s.initial_effect(c)
 	e4:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
 	e4:SetValue(2)
 	c:RegisterEffect(e4)
-end
+	--flip effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_FLIP)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetOperation(s.flipop)
+	c:RegisterEffect(e1)
+	--add
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.retcon)
+	e2:SetTarget(s.rettg)
+	e2:SetOperation(s.retop)
+	c:RegisterEffect(e2)
+end	
 s.listed_series={0xbb8}
-function s.filter(c)
+function s.efilter(c)
 	return c:IsSetCard(0xBB8) and c:IsAttribute(ATTRIBUTE_DARK)
 end
 function s.dircon(e)
 	return e:GetHandler():GetEquipTarget():GetAttackAnnouncedCount()>0
 end
+--add
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|(RESETS_STANDARD|RESET_OVERLAY)&~(RESET_LEAVE|RESET_TOGRAVE),0,0)
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)~=0
+end
+function s.filter(c,e,tp)
+	return c:GetCode()==id	and c:IsAbleToHand()
+end
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,c)
+	end
+end
+
