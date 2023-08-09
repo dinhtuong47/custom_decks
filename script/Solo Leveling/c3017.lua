5102a72dc83b2b4d102f802efb3ceadbdf68b105
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-09 18:14:37 +0700

    Update c3017.lua

--- script/Solo Leveling/c3017.lua
+++ script/Solo Leveling/c3017.lua
@@ -14,7 +14,16 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(aux.ritlimit)
 	c:RegisterEffect(e1)
-end
+	--flip
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1,id+50)
+	e3:SetOperation(s.setop)
+	c:RegisterEffect(e3)
+end	
 --cannot neg
 function s.immuop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
@@ -24,4 +33,9 @@ function s.immuop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(1)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 	e:GetHandler():RegisterEffect(e1)
+end
+--set all
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_ONFIELD,nil)
+	Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
 end
