6804f1fa24fe2cb42b9b2d40cc757c723c04e003
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-09 11:12:38 +0700

    Update c3010.lua

--- script/Solo Leveling/c3010.lua
+++ script/Solo Leveling/c3010.lua
@@ -28,45 +28,40 @@ function s.initial_effect(c)
 	e4:SetOperation(s.thop)
 	c:RegisterEffect(e4)
 end
---atkup
+--add
 local key=TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP
-function s.cffilter(c,ctype)
-	return c:IsSetCard(0xBB8) and not c:IsType(ctype&key)  and not c:IsPublic() 
+function s.togravefilter(c,ctype)
+	return c:IsSetCard(0xBB8) and not c:IsType(ctype&key) and c:IsAbleToGrave()
 end
 function s.tgfilter(c,tp)
-	return c:IsSetCard(0xBB8) and Duel.IsExistingMatchingCard(s.cffilter,tp,LOCATION_HAND,0,1,nil,c:GetType())
+	return c:IsFaceup() and c:IsSetCard(0xBB8) and Duel.IsExistingMatchingCard(s.togravefilter,tp,LOCATION_DECK,0,1,nil,c:GetType())
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_HAND) and chkc:IsControler(tp) and s.tgfilter(chkc,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_HAND,0,1,nil,tp) end
-	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) and s.tgfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_ONFIELD,0,1,nil,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,LOCATION_HAND)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.tohandfilter(c,type1,type2)
 	return c:IsSetCard(0xBB8) and not c:IsType(type1&key) and not c:IsType(type2&key) and not c:IsCode(id) and c:IsAbleToHand()
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	--Cannot Special Summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetTarget(s.splimit)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,1),nil)
-	
+	--Send to GY and Special Summon
 	if tc and tc:IsRelateToEffect(e) then
-		local g=Duel.SelectMatchingCard(tp,s.cffilter,tp,LOCATION_HAND,0,1,1,nil,tc:GetType())
-		if #g>0 and Duel.ConfirmCards(1-tp,g)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g=Duel.SelectMatchingCard(tp,s.togravefilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetType())
+		if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
 			local ogc=Duel.GetOperatedGroup():GetFirst()
-			if ogc:IsLocation(LOCATION_HAND) and c:IsRelateToEffect(e) then
+			if ogc:IsLocation(LOCATION_GRAVE) and c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
 				--Search
 				local gth=Duel.GetMatchingGroup(s.tohandfilter,tp,LOCATION_DECK,0,nil,tc:GetType(),g:GetFirst():GetType())
-				if #gth>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+				if #gth>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 					Duel.BreakEffect()
 					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 					local sg=gth:Select(tp,1,1,nil)
@@ -77,9 +72,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp)
-	return not (c:IsLevelAbove(3) or c:IsRankAbove(3))
-end
+
 
 
  
