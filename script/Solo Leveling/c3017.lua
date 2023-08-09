655e5fe071ae53ecc3a94a774dc554da1ee10823
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-09 18:12:13 +0700

    Update c3017.lua

--- script/Solo Leveling/c3017.lua
+++ script/Solo Leveling/c3017.lua
@@ -1,6 +1,12 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	--cannot be negated
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	e0:SetOperation(s.immuop)
+	c:RegisterEffect(e0)
 	--Special Summon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -8,4 +14,14 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(aux.ritlimit)
 	c:RegisterEffect(e1)
+end
+--cannot neg
+function s.immuop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE++EFFECT_FLAG_CANNOT_INACTIVATE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	e:GetHandler():RegisterEffect(e1)
 end
