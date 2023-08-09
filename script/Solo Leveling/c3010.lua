d8c9767d9028a7bc69ae5776cfee19a32a47733d
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-09 12:05:06 +0700

    Update c3010.lua

--- script/Solo Leveling/c3010.lua
+++ script/Solo Leveling/c3010.lua
@@ -31,36 +31,34 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 --add
-local key=TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP
-function s.cfilter1(c,tp)
-	return c:IsSetCard(0xBB8) and not c:IsPublic() --[[and Duel.IsExistingMatchingCard(s.cffilter2,tp,LOCATION_HAND,0,1,nil,c:GetCode()) ]]--
+function s.cfilter(c)
+	return c:IsSetCard(0xBB8) and not c:IsPublic()
 end
-function s.cfilter2(c,tp)
-	return c:IsSetCard(0xBB8) and not c:IsPublic() --[[and not c:IsCode(code)]]--
+function s.rescon(sg,e,tp,mg)
+	return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,sg)
 end
-function s.thfilter(c,ctype)
-	return c:IsSetCard(0xBB8) and c:IsAbleToHand() and not c:IsType(ctype&key)
+function s.thfilter(c,e,tp,sg)
+	return c:IsSetCard(0xBB8) and c:IsAbleToHand()  
+		and not sg:IsExists(Card.IsType,1,nil,c:GetType())
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cffilter1,tp,LOCATION_HAND,0,1,nil,tp) 
-			and Duel.IsExistingMatchingCard(s.cffilter2,tp,LOCATION_HAND,0,1,nil,tp) end
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) and g:GetClassCount(Card.GetCode)>=2 end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g1=Duel.GetMatchingGroup(s.cfilter1,tp,LOCATION_HAND,0,nil,e:GetHandler():GetType())
-	local g2=Duel.GetMatchingGroup(s.cfilter2,tp,LOCATION_HAND,0,nil,e:GetHandler():GetType())
-	if #g1>0 and #g2>0 then
-	local sg1=aux.SelectUnselectGroup(g1,e,tp,1,1,aux.dncheck,1,tp,HINTMSG_CONFIRM)
-	Duel.ConfirmCards(1-tp,sg1)
-	local sg2=aux.SelectUnselectGroup(g2,e,tp,1,1,aux.dncheck,1,tp,HINTMSG_CONFIRM)
-	Duel.ConfirmCards(1-tp,sg2)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil)
+	if #g<2 then return end
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_CONFIRM)
+	Duel.ConfirmCards(1-tp,sg)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local eq=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,sg1:GetType(),sg2:GetFirst():GetType())
+		local eq=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,sg):GetFirst()
 		if not eq then return end
 		Duel.BreakEffect()
 		Duel.SendtoHand(eq,tp,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,eq)
-	end
+	Duel.ConfirmCards(1-tp,eq)
 end
+
+
+ 
