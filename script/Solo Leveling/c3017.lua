fc98df21be9fcd4a5407bd39b79bc0cb8934af68
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-09 18:19:19 +0700

    Update c3017.lua

--- script/Solo Leveling/c3017.lua
+++ script/Solo Leveling/c3017.lua
@@ -14,6 +14,20 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(aux.ritlimit)
 	c:RegisterEffect(e1)
+	--quick eff
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e2:SetCountLimit(1,id+50)
+	e2:SetCost(s.poscost)
+	e2:SetCondition(s.poscon)
+	e2:SetOperation(s.posop)
+	c:RegisterEffect(e2)
 	--flip
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
@@ -33,6 +47,21 @@ function s.immuop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(1)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 	e:GetHandler():RegisterEffect(e1)
+end
+--quick eff
+function s.filter(c)
+	return c:IsSetCard(0xBB8) and c:IsDiscardable()
+end
+function s.poscost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,s.filter,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.poscon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase() and Duel.GetTurnPlayer()~=tp
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_ONFIELD,0,1,nil)
+	Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
 end
 --set all
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
