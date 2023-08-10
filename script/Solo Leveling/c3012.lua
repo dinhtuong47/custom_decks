83ce8c7d2482dcb6268b32c210fc220e96626799
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-10 22:37:12 +0700

    Update c3012.lua

--- script/Solo Leveling/c3012.lua
+++ script/Solo Leveling/c3012.lua
@@ -1,77 +1,81 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SUMMON)
+	e1:SetCategory(CATEGORY_SUMMON+CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--Flip & set from field
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_POSITION)
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
-	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e2:SetTarget(s.postg)
-	e2:SetOperation(s.posop)
-	c:RegisterEffect(e2)
-	local e3=e2:Clone()
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetTarget(s.postg2)
-	e3:SetOperation(s.posop2)
-	c:RegisterEffect(e3)
 end
---set from hand
-function s.filter(c)
-	return c:IsSetCard(0xBB8) and c:IsSummonable(true,nil)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
+function s.tg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.smfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.tg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.tg3(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter2,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc) end
+	local b1=s.tg1(e,tp,eg,ep,ev,re,r,rp,0)
+	local b2=s.tg2(e,tp,eg,ep,ev,re,r,rp,0)
+	local b3=s.tg3(e,tp,eg,ep,ev,re,r,rp,0)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)},
+		{b3,aux.Stringid(id,2)})
+	if op==1 then
+		e:SetProperty(0)
+		e:SetOperation(s.op1)
+		s.tg1(e,tp,eg,ep,ev,re,r,rp,1)
+	if op==2 then
+		e:SetProperty(0)
+		e:SetOperation(s.op2)
+		s.tg2(e,tp,eg,ep,ev,re,r,rp,1)
+	else
+		e:SetProperty(0)
+		e:SetOperation(s.op3)
+		s.tg3(e,tp,eg,ep,ev,re,r,rp,1)
+	end
+end
+--set from hand
+function s.smfilter(c,e,tp)
+	return c:IsSetCard(0xBB8) and c:IsSummonable(true,nil)
+end
+function s.op1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.smfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if tc then
 		Duel.MSet(tp,tc,true,nil)
 	end
 end
 --set from field
-function s.posfilter(c)
-	return c:IsSetCard(0xBB8) and c:IsFaceup() and c:IsCanChangePosition()
+function s.posfilter(c,e,tp)
+	return c:IsSetCard(0xBB8) and c:IsFaceup() and c:IsCanTurnSet()
 end
-function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
-end
-function s.posop(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.op2(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
 	end
 end
 --flip
-function s.posfilter2(c)
+function s.posfilter2(c,e,tp)
 	return c:IsSetCard(0xBB8) and c:IsFacedown() and c:IsCanChangePosition()
 end
-function s.postg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter2,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
-end
-function s.posop2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.op3(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local g=Duel.SelectMatchingCard(tp,s.posfilter2,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter2,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.ChangePosition(g,POS_FACEUP_ATTACK)
 	end
 end
-
