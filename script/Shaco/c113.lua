7fdaf0c2c46a81860c35c2d1744db338d7b63d44
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-08-16 22:15:19 +0700

    Update c113.lua

--- script/Shaco/c113.lua
+++ script/Shaco/c113.lua
@@ -24,7 +24,7 @@ function s.initial_effect(c)
 	--gain eff
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetCountLimit(1,id+50)
