14c3a89f67f7de0678b7423449d36fb7bd6843e8
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2023-10-30 22:20:56 +0700

    Update c3001.lua

--- script/Solo Leveling/c3001.lua
+++ script/Solo Leveling/c3001.lua
@@ -39,33 +39,37 @@ end
 function s.cfilter(c)
 	return c:IsSetCard(0xBB8) and not c:IsPublic()
 end	
-function s.filter(c,e,tp)
-	return (c:IsCode(3000) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE))
-	or (c:IsSetCard(0xBB8) and c:IsType(TYPE_SPELL+TYPE_TRAP))
+function s.spsetfilter(c,e,tp,ft)
+	return (ft>0 and c:IsCode(3000) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE))
+		or (c:IsSetCard(0xBB8) and c:IsType(TYPE_SPELL+TYPE_TRAP))
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.spsettarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local ft=Duel.GetMZoneCount(tp,c)
 	local rvg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and rvg:GetClassCount(Card.GetCode)>=2 
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_DECK)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and rvg:GetClassCount(Card.GetCode)>=2
+	and Duel.IsExistingMatchingCard(s.spsetfilter,tp,LOCATION_HAND,0,1,c,e,tp,ft) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	--[[if not e:GetHandler():IsRelateToEffect(e) then return end]]--
+function s.spsetop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local rvg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND,0,nil)
-	local g=aux.SelectUnselectGroup(rvg,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_CONFIRM)
-	if #g<2 then return end
+	local g=aux.SelectUnselectGroup(rvg,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_CONFturn end
 	Duel.ConfirmCards(1-tp,g)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
-		Duel.ConfirmCards(1-tp,g)
-	elseif Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
-	--set to field
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET) end
-		local sc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
-		if sc and Duel.SSet(tp,sc)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+	local sc=Duel.SelectMatchingCard(tp,s.spsetfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,ft):GetFirst()
+	if not sc then return end
+	if sc:IsMonster() then
+		Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+	elseif sc:IsTrap() and Duel.SSet(tp,sc)>0 then
+		--It can be activated this turn
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(id,3))
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+		e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		sc:RegisterEffect(e1)
 	end
 end
 --set
