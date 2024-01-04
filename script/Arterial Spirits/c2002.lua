f68fefde59788b6e22ae655e5ce003b7384f1266
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-04 17:01:41 +0700

    Update c2002.lua

--- script/Arterial Spirits/c2002.lua
+++ script/Arterial Spirits/c2002.lua
@@ -64,7 +64,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-		e1:SetValue(500)
+		e1:SetValue(1000)
 		c:RegisterEffect(e1)
 		end
 	end
