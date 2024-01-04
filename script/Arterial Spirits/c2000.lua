7fd39aedfed0e9c98d9bef1eaacf825812b16eda
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-04 16:50:28 +0700

    Update c2000.lua

--- script/Arterial Spirits/c2000.lua
+++ script/Arterial Spirits/c2000.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
 	--return to hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_TOGRAVE++CATEGORY_DRAW)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_TOGRAVE+CATEGORY_DRAW)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
