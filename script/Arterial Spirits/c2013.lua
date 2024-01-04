e4d423e0c4df2908132df8b78a736958ff2b1bf1
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-04 17:28:21 +0700

    Update c2013.lua

--- script/Arterial Spirits/c2013.lua
+++ script/Arterial Spirits/c2013.lua
@@ -2,6 +2,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
@@ -23,11 +24,13 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.SetLP(tp,Duel.GetLP(tp)+tc:GetDefense())
+	if tc and tc:IsRelateToEffect(e) and Duel.SetLP(tp,Duel.GetLP(tp)+tc:GetDefense())~=0 then
+		Duel.ConfirmCards(1-tp,c)
+		Duel.Draw(tp,1,REASON_EFFECT)
 		local c=e:GetHandler()
 	end
     if tc:IsCode(2004) and tc:IsRelateToEffect(e) then
