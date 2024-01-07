0d47f994780443544ac2c6805a46a6ac249d0db1
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-07 15:41:51 +0700

    Create 4012.lua

---
+++ script/Celestial/4012.lua
@@ -0,0 +1,37 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,s.ffilter,2)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+	aux.EnableNeosReturn(c)
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+end
+--material
+function s.ffilter(c,fc,sumtype,sp,sub,mg,sg)
+	return c:IsSetCard(0x254a,fc,sumtype,sp) and (not sg or sg:FilterCount(aux.TRUE,c)==0 or not sg:IsExists(Card.IsAttribute,1,c,c:GetAttribute(),fc,sumtype,sp))
+end
+--fus
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(Card.IsAbleToDeckOrExtraAsCost,tp,LOCATION_ONFIELD,0,nil)
+end
+function s.contactop(g,tp)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST+REASON_MATERIAL)
+end
+function s.splimit(e,se,sp,st)
+	return not e:GetHandler():IsLocation(LOCATION_EXTRA)
+end
+function s.atkval(e,c)
+	local lps=Duel.GetLP(c:GetControler())
+	local lpo=Duel.GetLP(1-c:GetControler())
+	if lps>=lpo then return 0
+	else return lpo-lps end
+end
