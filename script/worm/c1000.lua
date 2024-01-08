52c56f50071e29c2ca5f5a9d8929564429c270e5
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-08 20:18:47 +0700

    Update c1000.lua

--- script/worm/c1000.lua
+++ script/worm/c1000.lua
@@ -6,14 +6,15 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
 	--indestructable
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e2:SetRange(LOCATION_FZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(s.infilter)
-	e2:SetValue(1)
-	c:RegisterEffect(e2)
+	--indes
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(s.infilter)
+	e3:SetValue(s.indct)
+	c:RegisterEffect(e3)
     --change pos
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
@@ -41,6 +42,13 @@ end
 --INDES
 function s.infilter(e,c)
 	return c:IsLevelBelow(4) and c:IsRace(RACE_REPTILE) and c:IsSetCard(0x3e)
+end
+function s.indct(e,re,r,rp)
+	if (r&REASON_BATTLE)~=0 then
+		return 1
+	else
+		return 0
+	end
 end
 --pos
 function s.cfilter(c,tp)
