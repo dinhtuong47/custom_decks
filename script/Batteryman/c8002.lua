0ef8e2a05ef3fcc3ecbea38caf3de236821a58d4
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-09 18:06:59 +0700

    Update c8002.lua

--- script/Batteryman/c8002.lua
+++ script/Batteryman/c8002.lua
@@ -3,7 +3,7 @@ function s.initial_effect(c)
 	--Must be properly summoned before reviving
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,s.matfilter,2,2)
-	--Add from Deck to hand
+	--Add from Deck or GY to hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -42,23 +42,23 @@ function s.thcon(e)
 end
 function s.thfilter2(c,tp)
 	return  ( c:IsRace(RACE_THUNDER) and c:IsAttribute(ATTRIBUTE_LIGHT) ) and c:IsAbleToHand()
-		and Duel.IsExistingMatchingCard(s.thfilter3,tp,LOCATION_DECK,0,1,c)
+		and Duel.IsExistingMatchingCard(s.thfilter3,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,c)
 end
 function s.thfilter3(c)
 	return c:IsCode(61181383) or c:IsCode(99995595) or c:IsCode(49479374) or c:IsCode(8001) or c:IsCode(35100834) or c:IsCode(61840587) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK,0,1,nil,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g1=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK,0,1,1,nil,tp)
+	local g1=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp)
 	if #g1>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g2=Duel.SelectMatchingCard(tp,s.thfilter3,tp,LOCATION_DECK,0,1,1,g1:GetFirst())
+		local g2=Duel.SelectMatchingCard(tp,s.thfilter3,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,g1:GetFirst())
 		g1:Merge(g2)
 		Duel.SendtoHand(g1,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g1)
@@ -77,7 +77,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.splimit(e,c)
-	return not ( c:IsRace(RACE_THUNDER) and c:IsAttribute(ATTRIBUTE_LIGHT) )
+	return not c:IsSetCard(0x28)
 end
 --prevent
 function s.disop(e,tp)
