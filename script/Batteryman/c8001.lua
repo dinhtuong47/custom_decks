669af18448beef2a306876dfd15c8dc14b8403ee
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-11 13:28:07 +0700

    Update c8001.lua

--- script/Batteryman/c8001.lua
+++ script/Batteryman/c8001.lua
@@ -95,11 +95,11 @@ function s.thop3(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 --recycle
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thtg4(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop4(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
