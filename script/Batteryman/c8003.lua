c6e4969f5158d2684d4d05410905c218e9235fba
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-13 06:03:31 +0700

    Update c8003.lua

--- script/Batteryman/c8003.lua
+++ script/Batteryman/c8003.lua
@@ -2,7 +2,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--discard
   --Normal Summon/set
-local e2=Effect.CreateEffect(c)
+        local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
@@ -14,11 +14,34 @@ local e2=Effect.CreateEffect(c)
 	c:RegisterEffect(e2)
   --add from GY
 	local e5=e2:Clone()
-  e5:SetDescription(aux.Stringid(id,1))
+        e5:SetDescription(aux.Stringid(id,1))
 	e5:SetCategory(CATEGORY_TOHAND)
-  e5:SetTarget(s.thtg2)
+        e5:SetTarget(s.thtg2)
 	e5:SetOperation(s.thop2)
 	c:RegisterEffect(e5)
-
-
+end
+--SummonOrSet
+function s.thtg3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+end
+function s.thop3(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local tc=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.SummonOrSet(tp,tc,true,nil)
+	end
+end
+--recycle
+function s.thtg4(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thop4(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
