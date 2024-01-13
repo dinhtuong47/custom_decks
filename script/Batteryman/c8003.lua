a2c0676e243fab80cc66e6a01f818fef7422b300
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-13 16:34:07 +0700

    Update c8003.lua

--- script/Batteryman/c8003.lua
+++ script/Batteryman/c8003.lua
@@ -87,7 +87,6 @@ function s.thfilter(c)
 end
 	--Activation legality
 function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
