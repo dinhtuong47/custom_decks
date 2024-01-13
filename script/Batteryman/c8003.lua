8685914b4de3081b477b8bac2bac62c4bd81eb7e
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-13 07:43:25 +0700

    Update c8003.lua

--- script/Batteryman/c8003.lua
+++ script/Batteryman/c8003.lua
@@ -41,12 +41,13 @@ function s.sumfilter(c)
 	return c:IsRace(RACE_THUNDER)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil,e:GetHandler()) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND,0,1,nil,e:GetHandler()) end
 	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-	local tc=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil,e:GetHandler()):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND,0,1,1,nil,e:GetHandler()):GetFirst()
 	if tc then
 		Duel.SummonOrSet(tp,tc,true,nil)
 	end
