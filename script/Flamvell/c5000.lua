cabadcdf1399972300f80399c06df81076494300
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-18 21:42:24 +0700

    Update c5000.lua

--- script/Flamvell/c5000.lua
+++ script/Flamvell/c5000.lua
@@ -45,8 +45,8 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		--Banish it when it leaves the field
+	if c:IsRelateToEffect(e) then Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		--[[Banish it when it leaves the field
 		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(3300)
 		e1:SetType(EFFECT_TYPE_SINGLE)
@@ -54,7 +54,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
 		e1:SetValue(LOCATION_REMOVED)
 		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
-		c:RegisterEffect(e1,true)
+		c:RegisterEffect(e1,true)]]--
 	end
 end
 
@@ -79,8 +79,8 @@ function s.posop(e,tp,ev,eg,ev,re,r,rp)
 		if tc:IsPosition(POS_FACEUP_DEFENSE) then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_SET_DEFENSE_FINAL)
-			e1:SetValue(200)
+			e1:SetCode(EFFECT_UPDATE_DEFENSE)
+			e1:SetValue(-1000)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			tc:RegisterEffect(e1)
 		end
