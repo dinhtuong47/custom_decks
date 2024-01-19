f109eb6fcfc697da7f69f335317a7baefb691358
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-19 17:14:15 +0700

    Update c5001.lua

--- script/Flamvell/c5001.lua
+++ script/Flamvell/c5001.lua
@@ -51,7 +51,7 @@ end
 --double dmg
 function s.actcon2(e)
 	local gct=Duel.GetFieldGroupCount(e:GetHandler():GetControler(),0,LOCATION_GRAVE)
-	return gct<=5 end
+	return gct<=5
 end
 function s.damtg(e,c)
 	return c:IsType(TYPE_SYNCHRO) and c:IsSetCard(0x2c) and c:GetBattleTarget()~=nil
@@ -59,7 +59,7 @@ end
 --atk boost
 function s.actcon3(e)
 	local gct=Duel.GetFieldGroupCount(e:GetHandler():GetControler(),0,LOCATION_GRAVE)
-	return gct<=7 end
+	return gct<=7
 end
 function s.atkval(e,c)
 	local tp=e:GetHandlerPlayer()
