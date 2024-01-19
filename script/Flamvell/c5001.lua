d4871c055bc171f4cf9e2ed1cbeaecc9f287a90f
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-01-19 18:08:03 +0700

    Update c5001.lua

--- script/Flamvell/c5001.lua
+++ script/Flamvell/c5001.lua
@@ -5,15 +5,15 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
-	--Additional Normal Summon
+	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_SZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x2c))
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 	--act limit
 	local e2=Effect.CreateEffect(c)
@@ -55,6 +55,24 @@ function s.initial_effect(c)
 	e5:SetCondition(s.atcon4)
 	e5:SetValue(s.atkval)
 	c:RegisterEffect(e5)
+end
+--ss from hand
+function s.filter(c,e,sp)
+	return c:IsSetCard(0x2c) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
 --act limit
 function s.actcon(e)
