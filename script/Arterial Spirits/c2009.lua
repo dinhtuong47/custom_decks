f40505e98be2ef9299590d96bfba01d2cfd8bab5
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-04-29 12:21:17 +0700

    Update c2009.lua

--- script/Arterial Spirits/c2009.lua
+++ script/Arterial Spirits/c2009.lua
@@ -17,17 +17,17 @@ function s.initial_effect(c)
 	local e4=e1:Clone()
 	e4:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e4)
-	--Cannot target
+	--Activation limit
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetTargetRange(0,1)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_ONFIELD,0)
-	e2:SetCondition(s.indcon)
-	e2:SetTarget(s.cannottgeff)
-	e2:SetValue(aux.tgoval)
+	e2:SetCondition(s.actcon)
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
+
 	--Reaveal and add
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
@@ -48,12 +48,12 @@ end
 function s.indfilter(c)
 	return c:IsFaceup() and c:IsLevel(6) and c:IsSetCard(0X7D0) 
 end
-function s.indcon(e)
-	return Duel.IsExistingMatchingCard(s.indfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+function s.actcon(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsBattlePhase() and 
+Duel.IsExistingMatchingCard(s.indfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
-function s.cannottgeff(e,c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP)  
-end
+
 --reveal
 function s.cfilter(c,tp)
 	return c:IsMonster() and c:IsSetCard(0x7D0) and not c:IsPublic() and c:IsAbleToDeck()
