c7c3f5bb347cb1a366b2fdbf313845b4015b93fb
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-05-20 18:11:18 +0700

    Update c11003.lua

--- script/Bonding/c11003.lua
+++ script/Bonding/c11003.lua
@@ -28,36 +28,36 @@ function s.cfilter(c)
 	return c:IsCode(45898858) and c:IsAbleToRemoveAsCost()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.thfilter2(c,tp)
-	return (c:IsFaceup() or c:IsLocation(LOCATION_HAND+LOCATION_DECK)) and c:IsCode(22587018) and c:IsAbleToGrave()
+	return (c:IsFaceup() or c:IsLocation(LOCATION_HAND) and c:IsCode(22587018) and c:IsAbleToGrave()
 	--[[and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,c)]]--
-		and Duel.IsExistingMatchingCard(s.thfilter3,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,1,c)
+		and Duel.IsExistingMatchingCard(s.thfilter3,tp,LOCATION_HAND+LOCATION_MZONE,0,1,c)
 end
 function s.thfilter3(c)
-	return (c:IsFaceup() or c:IsLocation(LOCATION_HAND+LOCATION_DECK)) and c:IsCode(58071123) and c:IsAbleToGrave()
+	return (c:IsFaceup() or c:IsLocation(LOCATION_HAND) and c:IsCode(58071123) and c:IsAbleToGrave()
 end
 function s.filter(c,e,tp)
 	return c:IsCode(85066822) and c:IsCanBeSpecialSummoned(e,0,tp,true,true,POS_FACEUP)
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	and Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,2,nil,tp)
-	and Duel.IsExistingMatchingCard(s.thfilter3,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,1,nil,tp)
+	and Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_HAND+LOCATION_MZONE,0,2,nil,tp)
+	and Duel.IsExistingMatchingCard(s.thfilter3,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil,tp)
 	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND+LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g1=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,2,2,nil,tp)
+	local g1=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_HAND+LOCATION_MZONE,0,2,2,nil,tp)
 	if #g1>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local g2=Duel.SelectMatchingCard(tp,s.thfilter3,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_MZONE,0,1,1,g1:GetFirst())
+		local g2=Duel.SelectMatchingCard(tp,s.thfilter3,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,g1:GetFirst())
 		g1:Merge(g2)
 		Duel.SendtoGrave(g1,tp,nil,REASON_EFFECT)
     end
