02e59b3cc12aaedf0613dfb7afa760e04506ac9e
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-05-21 01:14:37 +0700

    Update c11001.lua

--- script/Bonding/c11001.lua
+++ script/Bonding/c11001.lua
@@ -75,7 +75,7 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
-		Duel.SendtoGrave(g,nil,REASON_EFFECT)
+		Duel.SendtoGrave(g,REASON_EFFECT)
 	end
 end
 
