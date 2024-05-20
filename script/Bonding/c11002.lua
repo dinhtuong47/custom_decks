1f4ee6da8d801eacc011f982e51b8829539b8b64
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-05-20 15:49:31 +0700

    Create c11002.lua

---
+++ script/Bonding/c11002.lua
@@ -0,0 +1,21 @@
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+  --send+atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x100}
+  
