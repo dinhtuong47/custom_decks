20e39998920a4173567ad0420eb590a6e4a32d97
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-05-20 15:58:43 +0700

    Update c11002.lua

--- script/Bonding/c11002.lua
+++ script/Bonding/c11002.lua
@@ -11,10 +11,11 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_FZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 --inactivatable
 	local e4=Effect.CreateEffect(c)
@@ -31,6 +32,37 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 s.listed_series={0x100}
+--sent and gain atk
+function s.filter1(c)
+	return c:IsFaceup() and c:IsRace(RACE_DINOSAUR)
+end
+function s.filter2(c)
+	return (c:IsRace(RACE_SEASERPENT) and c:IsLevelAbove(8)) or (c:IsSetCard(0x100)) and c:IsAbleToGrave()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATIONN_HAND+LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATIONN_HAND+LOCATION_DECK)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATIONN_HAND+LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		if Duel.SendtoGrave(gc,REASON_EFFECT)~=0 and tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetValue(800)
+			tc:RegisterEffect(e1)
+		end
+	end
+end
+--immu
 function s.effectfilter(e,ct)
 	local p=e:GetHandler():GetControler()
 	local te,tp,loc=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_LOCATION)
