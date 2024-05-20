e3a88d66782f5a84c8a32e823e935b1888b0157d
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-05-20 15:51:21 +0700

    Update c11002.lua

--- script/Bonding/c11002.lua
+++ script/Bonding/c11002.lua
@@ -16,6 +16,24 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+--inactivatable
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_INACTIVATE)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetValue(s.effectfilter)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_DISEFFECT)
+	e5:SetRange(LOCATION_FZONE)
+	e5:SetValue(s.effectfilter)
+	c:RegisterEffect(e5)
 end
 s.listed_series={0x100}
+function s.effectfilter(e,ct)
+	local p=e:GetHandler():GetControler()
+	local te,tp,loc=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_LOCATION)
+	return p==tp and te:GetHandler():IsSetCard(0x100) and loc&LOCATION_ONFIELD~=0
+end
   
