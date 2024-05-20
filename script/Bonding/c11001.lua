1041b0d503faa3bcd7a3f8ccfaea64e09776cb39
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-05-20 15:32:48 +0700

    Update c11001.lua

--- script/Bonding/c11001.lua
+++ script/Bonding/c11001.lua
@@ -7,6 +7,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
@@ -23,6 +24,12 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 --add
+function s.thconfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_SEASERPENT)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(s.thconfilter,tp,LOCATION_MZONE,0,1,nil)
+end
 function s.thfilter1(c,tp)
 	return c:IsSetCard(0x100) and not c:IsPublic() 
 		and Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,2,nil,c)
@@ -57,11 +64,11 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP) end
 end
 --set
-function s.spconfilter(c)
+function s.setconfilter(c)
 	return c:IsMonster() or c:GetPreviousTypeOnField()&TYPE_MONSTER==TYPE_MONSTER
 end
 function s.setcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.spconfilter,1,nil)
+	return eg:IsExists(s.setconfilter,1,nil)
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
