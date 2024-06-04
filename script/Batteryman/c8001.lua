b5dcacaec3f8ce14368c1a02856317e6cc8ac7ee
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-06-04 11:48:56 +0700

    Update c8001.lua

--- script/Batteryman/c8001.lua
+++ script/Batteryman/c8001.lua
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 	--broken line
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DAMAGE)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetCountLimit(1,id)
@@ -30,6 +30,7 @@ function s.initial_effect(c)
         local e3=e2:Clone()
 	e3:SetDescription(aux.Stringid(id,1))
         e3:SetCountLimit(1,id+20)
+	e3:SetCondition(s.thcon)
 	e3:SetTarget(s.thtg2)
 	e3:SetOperation(s.thop2)
         c:RegisterEffect(e3)	
@@ -57,7 +58,7 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE,0,1,nil,20529766)
 			and Duel.IsExistingMatchingCard(s.blfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+	--[[Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)]]--
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
@@ -65,8 +66,8 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
-		Duel.BreakEffect()
-		Duel.Damage(1-tp,500,REASON_EFFECT)
+		--[[Duel.BreakEffect()
+		Duel.Damage(1-tp,500,REASON_EFFECT)]]--
 	end
 end
 --Short Circuit
@@ -74,7 +75,7 @@ function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE,0,1,nil,19441018)
 			and Duel.IsExistingMatchingCard(s.scfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,1000)
+	--[[Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,1000)]]--
 end
 function s.thop2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
@@ -82,8 +83,8 @@ function s.thop2(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
-		Duel.BreakEffect()
-		Duel.Damage(tp,1000,REASON_EFFECT)
+		--[[Duel.BreakEffect()
+		Duel.Damage(tp,1000,REASON_EFFECT)]]--
 	end
 end
 
