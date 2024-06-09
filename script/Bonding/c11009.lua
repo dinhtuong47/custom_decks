d511ac2bbea8e80cde149f1186cf795b84a14a5e
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-06-09 22:38:12 +0700

    Update c11009.lua

--- script/Bonding/c11009.lua
+++ script/Bonding/c11009.lua
@@ -7,6 +7,7 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.flipcon)
 	e1:SetOperation(s.flipop)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
 	c:RegisterEffect(e1)
 end
 s.listed_names={22587018,58071123,15981690,62397231}
@@ -20,7 +21,7 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetDecktopGroup(tp,4)
 	local break_chk=false
-	--"Carbonnedon": 1 "Hyozanryu" you control gains 1000 ATK until the end of this turn
+	--"Carbonnedon": 1 "Hyozanryu" you control gains 1000 ATK
 	if g:IsExists(Card.IsCode,1,nil,15981690) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
 		local tc=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsCode,62397231),tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
@@ -32,11 +33,11 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetValue(1000)
-			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 			tc:RegisterEffect(e1)
 		end
 	end
-	--"Hydrogeddon": 1 "Hyozanryu" you control becomes unaffected by your opponent's monster effects until the end of this turn
+	--"Hydrogeddon": 1 "Hyozanryu" you control becomes unaffected by your opponent's card effects until the end of this turn
 	if g:IsExists(Card.IsCode,1,nil,22587018) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
 		local sc=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsCode,62397231),tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
@@ -46,24 +47,25 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 			break_chk=true
 			--Unaffected by your opponent's monster effects
 			local e2=Effect.CreateEffect(c)
-			e2:SetDescription(3111)
+			e2:SetDescription(3110)
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 			e2:SetCode(EFFECT_IMMUNE_EFFECT)
 			e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
-			e2:SetValue(function(e,te) return te:IsMonsterEffect() and te:GetOwnerPlayer()==1-e:GetHandlerPlayer() end)
+			e2:SetValue(function(e,te)  te:GetOwnerPlayer()==1-e:GetHandlerPlayer() end) --[[and return te:IsMonsterEffect()]]--
 			sc:RegisterEffect(e2)
 		end
 	end
 	--"Oxygeddon": Destroy 1 monster you opponent controls, then inflict 800 damage to both
 	if g:IsExists(Card.IsCode,1,nil,58071123) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local dg=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsMonster),tp,0,LOCATION_MZONE,1,1,nil)
+		local dg=Duel.SelectMatchingCard(tp,Card.IsMonster,tp,0,LOCATION_MZONE,1,1,nil)
 		if #dg>0 then
 			Duel.HintSelection(dg,true)
 			if break_chk then Duel.BreakEffect() end
 			if Duel.Destroy(dg,REASON_EFFECT)>0 then
 				Duel.BreakEffect()
+				Duel.Damage(tp,800,REASON_EFFECT)
 				Duel.Damage(1-tp,800,REASON_EFFECT)
 			end
 		end
