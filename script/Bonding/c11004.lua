6118a805a6752710d5258716bb95be85e4eb8fad
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2024-06-09 15:27:06 +0700

    Update c11004.lua

--- script/Bonding/c11004.lua
+++ script/Bonding/c11004.lua
@@ -1,4 +1,4 @@
---diamond breakdown
+--tidal blast
 local s,id=GetID()
 function s.initial_effect(c)
 	--activate
@@ -6,8 +6,11 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--spsummon
+	--[[spsummon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -19,25 +22,53 @@ function s.initial_effect(c)
 	e2:SetCost(s.cost)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
-	c:RegisterEffect(e2)
+	c:RegisterEffect(e2)]]--
 	--Return and draw 1 card
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_GRAVE)
 	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1,id)
-	e3:SetHintTiming(0,TIMING_END_PHASE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCost(aux.bfgcost)
 	e3:SetTarget(s.tdtg)
 	e3:SetOperation(s.tdop)
 	c:RegisterEffect(e3)
 	
 end
 s.listed_series={0x100}
-s.listed_names={62397231}
---special summon
+s.listed_names={85066822}
+--channge race
+function s.zfilter(c)
+	return c:IsFaceup() and c:IsCode(85066822)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.zfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.chfilter(c)
+	return c:IsFaceup()  
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.chfilter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.chfilter,tp,0,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)	
+	e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetValue(ATTRIBUTE_FIRE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	tc:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CHANGE_RACE)
+	e2:SetValue(RACE_PYRO)
+	tc:RegisterEffect(e2) 
+	end
+end
+--[[special summon
 function s.thcfilter(c,tp)
 	return c:IsRace(RACE_SEASERPENT) or c:IsCode(62397231) and c:IsAbleToDeckAsCost()
 end
@@ -65,10 +96,10 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
-end
+end]]--
 --draw
 function s.tdfilter(c)
-	return c:IsRace(RACE_DINOSAUR) and c:IsAbleToDeck()
+	return c:IsRace(RACE_DINOSAUR) or c:IsRace(RACE_SEASERPENT) and c:IsAbleToDeck()
 end
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c:IsControler(tp) and s.tdfilter(chkc) end
