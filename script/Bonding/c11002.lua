95f8d5bb977a7d7505d189912ab721cee7f8572d
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2025-05-13 01:40:21 +0700

    Update c11002.lua

--- script/Bonding/c11002.lua
+++ script/Bonding/c11002.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
-  --send+atk
+  --send+add
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND)
@@ -17,18 +17,17 @@ function s.initial_effect(c)
 	e1:SetTarget(s.tg)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
---ADD TO HAND
+--set
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetRange(LOCATION_FZONE)
 	e3:SetCode(EVENT_PHASE+PHASE_END)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetCountLimit(1,id+50)
-	e3:SetCondition(s.thcon)
-	e3:SetTarget(s.thdtg)
-	e3:SetOperation(s.thdop)
+	e3:SetCondition(s.scon)
+	e3:SetTarget(s.stg)
+	e3:SetOperation(s.sop)
 	c:RegisterEffect(e3)
 --inactivatable
 	local e4=Effect.CreateEffect(c)
@@ -70,35 +69,40 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 
 end
 --add
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+function s.scon(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer()
 end
-function s.thdfilter(c)
-	return  c:IsSetCard(0x100) and c:IsFaceup()  and (c:IsAbleToHand() or c:IsAbleToDeck())
-end
-function s.thdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
-	local rg=Duel.GetMatchingGroup(s.thdfilter,tp,LOCATION_REMOVED,0,nil,e)
-	if chk==0 then return aux.SelectUnselectGroup(rg,e,tp,2,2,nil,0) end
-	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,nil,1,tp,aux.Stringid(id,1))
-	Duel.SetTargetCard(g)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-end
-function s.thdop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetTargetCards(e)
-	if #g==2 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local hg=g:FilterSelect(tp,Card.IsAbleToHand,1,1,nil)
-		if #hg==0 or Duel.SendtoHand(hg,nil,REASON_EFFECT)==0 then return end
-		Duel.ConfirmCards(1-tp,hg)
-		local dg=g-hg
-		if #dg==0 then return end
-		Duel.HintSelection(dg,true)
-		Duel.SendtoDeck(dg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
-	end
+function s.setfilter(c)
+
+	return c:IsSetCard(0x100) and c:IsFaceup() and c:IsSSetable()
+
 end
 
+function s.stg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and s.setfilter(chkc) end
+
+	if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,LOCATION_REMOVED,0,1,nil) end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+
+	local tc=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_REMOVED,tc,1,0,0)
+
+end
+
+function s.sop(e,tp,eg,ep,ev,re,r,rp)
+
+	local tc=Duel.GetFirstTarget()
+
+	if tc and tc:IsRelateToEffect(e) then
+
+		Duel.SSet(tp,tc)
+
+	end
+
+end
 --immu
 function s.effectfilter(e,ct)
 	local p=e:GetHandler():GetControler()
