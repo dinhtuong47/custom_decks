6a666702efecf24b3914ad5b429cf1cf24cf9e61
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2025-07-15 23:28:04 +0700

    Update c2014.lua

--- script/Arterial Spirits/c2014.lua
+++ script/Arterial Spirits/c2014.lua
@@ -1,67 +1,140 @@
 local s,id=GetID()
+
 function s.initial_effect(c)
+
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_BATTLE_END)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+
 end
-s.listed_series={0x7D0}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsMainPhase() and eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp)
-end
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0x7D0) and c:IsLevel(4)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-	    Duel.BreakEffect()
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(1000)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-        	--The activation and effects of your Fish monsters on your field cannot be negated
-		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(id,2))
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-		e1:SetCode(EFFECT_CANNOT_INACTIVATE)
-		e1:SetTargetRange(1,0)
-		e1:SetValue(s.effectfilter)
-		e1:SetReset(RESET_PHASE|PHASE_END)
-		Duel.RegisterEffect(e1,tp)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_CANNOT_DISEFFECT)
-		Duel.RegisterEffect(e2,tp)
-		end
-		Duel.BreakEffect()
-		if tc:IsCode(2000) and Duel.IsPlayerCanDraw(1-tp,1) and tc:IsRelateToEffect(e) then
-		Duel.BreakEffect()
-		--end main phase
-		Duel.Draw(1-tp,1,REASON_EFFECT)
-		Duel.SkipPhase(Duel.GetTurnPlayer(),Duel.GetCurrentPhase(),RESET_PHASE|PHASE_END,1)
+
+	if Duel.IsTurnPlayer(1-tp) or Duel.GetCurrentPhase()~=PHASE_BATTLE or Duel.GetCurrentChain(true)>0 then
+
+		return false
+
 	end
-end
-function s.effectfilter(e,ct)
-	local p=e:GetHandlerPlayer()
-	local te,tp,loc=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_LOCATION)
-	return p==tp and (loc&LOCATION_HAND)~=0 and te:GetHandler():IsSetCard(0x7D0)
+
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+
+	return #g>0 and g:FilterCount(aux.FaceupFilter(Card.IsSetCard,0x7D0),nil)==#g
+
 end
 
- 
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+
+	if chk==0 then return true end
+
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+
+		Duel.SetChainLimit(aux.FALSE)
+
+	end
+
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+
+	local c=e:GetHandler()
+
+	if c:IsRelateToEffect(e) then
+
+		Duel.SendtoGrave(c,REASON_EFFECT)
+
+	end
+
+	--skip phases until Battle Phase
+
+	Duel.SkipPhase(tp,PHASE_BATTLE,RESET_PHASE|PHASE_END,1)
+
+	--prevent activations for the rest of that battle phase
+
+	local e0=Effect.CreateEffect(c)
+
+	e0:SetType(EFFECT_TYPE_FIELD)
+
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+
+	e0:SetCode(EFFECT_CANNOT_ACTIVATE)
+
+	e0:SetTargetRange(0,1)
+
+	e0:SetValue(1)
+
+	e0:SetReset(RESET_PHASE|PHASE_BATTLE|PHASE_END)
+
+	Duel.RegisterEffect(e0,tp)
+
+	Duel.SkipPhase(tp,PHASE_MAIN2,RESET_PHASE|PHASE_END,1)
+
+	Duel.SkipPhase(tp,PHASE_END,RESET_PHASE|PHASE_END,1)
+
+	local e1=Effect.CreateEffect(c)
+
+	e1:SetType(EFFECT_TYPE_FIELD)
+
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+
+	e1:SetCode(EFFECT_SKIP_TURN)
+
+	e1:SetTargetRange(0,1)
+
+	e1:SetReset(RESET_PHASE|PHASE_END|RESET_OPPO_TURN)
+
+	Duel.RegisterEffect(e1,tp)
+
+	Duel.SkipPhase(tp,PHASE_DRAW,RESET_PHASE|PHASE_END,2)
+
+	Duel.SkipPhase(tp,PHASE_STANDBY,RESET_PHASE|PHASE_END,2)
+
+	Duel.SkipPhase(tp,PHASE_MAIN1,RESET_PHASE|PHASE_END,2)
+
+	local e2=Effect.CreateEffect(c)
+
+	e2:SetType(EFFECT_TYPE_FIELD)
+
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+
+	e2:SetCode(EFFECT_CANNOT_EP)
+
+	e2:SetTargetRange(1,0)
+
+	e2:SetReset(RESET_PHASE|PHASE_MAIN1|RESET_SELF_TURN)
+
+	Duel.RegisterEffect(e2,tp)
+
+	--prevent activation of "Mischief of the Time Goddess"
+
+	local e3=Effect.CreateEffect(c)
+
+	e3:SetType(EFFECT_TYPE_FIELD)
+
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+
+	e3:SetDescription(aux.Stringid(id,1))
+
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+
+	e3:SetTargetRange(1,0)
+
+	e3:SetValue(s.limit)
+
+	e3:SetReset(RESET_PHASE|PHASE_END,3)
+
+	Duel.RegisterEffect(e3,tp)
+
+end
+
+function s.limit(e,re,tp)
+
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsCode(id)
+
+end
+
