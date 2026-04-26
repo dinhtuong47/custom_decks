29f71ce27f79845364ce5089e3340c39e7d78780
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-04-26 21:36:24 +0700

    Update c20001.lua

--- script/Dual Avatar/c20001.lua
+++ script/Dual Avatar/c20001.lua
@@ -85,7 +85,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
 	if ct>0 then
 	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_REMOVED,0,nil)
-		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 			local sg=g:Select(tp,1,1,nil)
 			Duel.BreakEffect()
