9e2c5c6fa081604895c3a247a135de7995fd4002
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-04-26 20:05:49 +0700

    Update c20003.lua

--- script/Dual Avatar/c20003.lua
+++ script/Dual Avatar/c20003.lua
@@ -25,15 +25,15 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-function s.cdesfilter(c)
+function s.fmfilter(c)
 	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x14f) and c:IsFaceup() and c:GetFlagEffect(85360035)~=0
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re)
 		and Duel.Destroy(eg,REASON_EFFECT)~=0 
-		and Duel.IsExistingMatchingCard(s.cdesfilter,tp,LOCATION_ONFIELD,0,1,nil) then
+		and Duel.IsExistingMatchingCard(s.fmfilter,tp,LOCATION_MZONE,0,1,nil) then
 local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,aux.ExceptThisCard(e))
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.HintSelection(sg)
