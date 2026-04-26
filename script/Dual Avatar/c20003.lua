8a4500b96de5a5a43d0059c1beb1e1149940614f
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-04-26 22:16:38 +0700

    Update c20003.lua

--- script/Dual Avatar/c20003.lua
+++ script/Dual Avatar/c20003.lua
@@ -4,7 +4,6 @@ function s.initial_effect(c)
 	--set
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,2))
-	e2:SetCategory(CATEGORY_SSET)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCode(EVENT_FREE_CHAIN)
