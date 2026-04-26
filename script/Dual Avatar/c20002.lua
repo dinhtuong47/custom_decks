bed5f2b1899dffd75fb6dbe13516ca641d3e42f4
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-04-26 19:03:10 +0700

    Update c20002.lua

--- script/Dual Avatar/c20002.lua
+++ script/Dual Avatar/c20002.lua
@@ -42,7 +42,9 @@ function s.setfilter(c)
 	return c:IsSetCard(0x14e) and (c:IsType(TYPE_QUICKPLAY) or c:IsType(TYPE_TRAP)) and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND,0,1,nil) 
+	e:GetHandler():RegisterFlagEffect(id,RESET_CHAIN,0,1) end
+	
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
