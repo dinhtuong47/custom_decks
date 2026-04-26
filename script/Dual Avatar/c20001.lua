a0be2c42faa09bc7962ef6c9aab631f7f4b00922
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-04-26 18:46:30 +0700

    Update c20001.lua

--- script/Dual Avatar/c20001.lua
+++ script/Dual Avatar/c20001.lua
@@ -1,1 +1,49 @@
-
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsSetCard(0x14e) and c:IsType(TYPE_MONSTER) 
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		local ct=Duel.GetCurrentChain()
+		if ct<2 then return end
+		local te,tep=Duel.GetChainInfo(ct-1,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
+		if tep==1-tp and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+			Duel.BreakEffect()
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_IMMUNE_EFFECT)
+			e1:SetTargetRange(LOCATION_MZONE,0)
+			e1:SetTarget(s.etarget)
+			e1:SetValue(s.efilter)
+			e1:SetLabelObject(te)
+			e1:SetReset(RESET_EVENT+RESET_CHAIN)
+			Duel.RegisterEffect(e1,tp)
+		end
+	end
+end
+function s.efilter(e,re)
+	return re==e:GetLabelObject()
+end
+function s.etarget(e,c)
+	return not c:IsType(TYPE_MONSTER) 
+end
