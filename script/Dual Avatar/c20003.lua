f15b06ed70c498ec80897109f86f65f4d9b0d039
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-04-26 18:35:57 +0700

    Update c20003.lua

--- script/Dual Avatar/c20003.lua
+++ script/Dual Avatar/c20003.lua
@@ -1,1 +1,50 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter1(c)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+		and Duel.IsExistingMatchingCard(s.cfilter1,tp,LOCATION_MZONE,0,2,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
 
+function s.cdesfilter(c)
+	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x14e) and c:IsFaceup() and c:GetFlagEffect(85360035)~=0
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re)
+		and Duel.Destroy(eg,REASON_EFFECT)~=0 and Duel.IsExistingMatchingCard(s.cdesfilter,tp,LOCATION_MZONE,0,1,nil) 
+    and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.BreakEffect()
+			Duel.HintSelection(g)
+			Duel.Destroy(g,REASON_EFFECT)
+		end
+	end
+end
