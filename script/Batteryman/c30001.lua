5d983fac39f697f547e93eb6413e4f7ab43c117c
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-05-02 13:04:55 +0700

    Update c30001.lua

--- script/Batteryman/c30001.lua
+++ script/Batteryman/c30001.lua
@@ -106,6 +106,15 @@ end
 --draw
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=eg:GetFirst()
-	return c:IsOnField() and c:IsRace(RACE_PSYCHIC)
+	return c:IsOnField() and c:IsType(TYPE_MONSTER)
 end
-
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
