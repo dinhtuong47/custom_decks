3da7db68b0848679a16f3f519fb6cd55f2036033
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-05-04 00:28:56 +0700

    Update c30001.lua

--- script/Batteryman/c30001.lua
+++ script/Batteryman/c30001.lua
@@ -2,7 +2,7 @@ local s,id=GetID()
 function s.initial_effect(c)
     -- Summon itself by Tribute Summon (Action từ tay)
     local e0=Effect.CreateEffect(c)
-    e0:SetDescription(aux.Stringid(id,4)) -- Thêm desc cho dễ nhìn
+    e0:SetDescription(aux.Stringid(id,4))
     e0:SetCategory(CATEGORY_SUMMON)
     e0:SetType(EFFECT_TYPE_IGNITION)
     e0:SetRange(LOCATION_HAND)
@@ -28,14 +28,14 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
--- Logic Triệu hồi
+-- Logic Triệu hồi: Kiểm tra có Thunder trên sân để active effect từ tay
 function s.tscon(e,tp,eg,ep,ev,re,r,rp)
     return Duel.IsMainPhase() and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_THUNDER),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
 
 function s.tstg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
-    if chk==0 then return c:IsSummonable(true,nil,1) end -- Dùng IsSummonable chuẩn hơn
+    if chk==0 then return c:IsSummonable(true,nil,1) end 
     Duel.SetOperationInfo(0,CATEGORY_SUMMON,c,1,0,0)
 end
 
@@ -46,23 +46,18 @@ function s.tsop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Kiểm tra nguyên liệu
+-- Kiểm tra nguyên liệu (Bitwise Flag)
 function s.valcheck(e,c)
-	local g=c:GetMaterial()
-	local flag=0
-	for tc in g:Iter() do
-		local code=tc:GetCode()
-		if code==55401221 then
-			flag=(flag|0x1)
-		elseif code==19733961 then
-			flag=(flag|0x2)
-		elseif code==63142001 then
-			flag=(flag|0x4)
-		elseif code==47346845 then
-            		flag=(flag|0x8)
-		end
-	end
-	e:SetLabel(flag)
+    local g=c:GetMaterial()
+    local flag=0
+    for tc in g:Iter() do
+        local code=tc:GetCode()
+        if code==55401221 then flag=(flag|0x1) end
+        if code==19733961 then flag=(flag|0x2) end
+        if code==63142001 then flag=(flag|0x4) end
+        if code==47346845 then flag=(flag|0x8) end
+    end
+    e:SetLabel(flag)
 end
 
 function s.regcon(e,tp,eg,ep,ev,re,r,rp)
@@ -73,7 +68,7 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
     local flag=e:GetLabelObject():GetLabel()
     local c=e:GetHandler()
     
-    -- Effect 1: Negate Spell
+    -- Effect 1: Negate Spell (Thêm Once Per Turn)
     if (flag&0x1)~=0 then
         local e1=Effect.CreateEffect(c)
         e1:SetDescription(aux.Stringid(id,0))
@@ -81,6 +76,7 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
         e1:SetType(EFFECT_TYPE_QUICK_O)
         e1:SetCode(EVENT_CHAINING)
         e1:SetRange(LOCATION_MZONE)
+        e1:SetCountLimit(1) -- Tránh spam vô hạn
         e1:SetCondition(s.discon)
         e1:SetTarget(s.distg)
         e1:SetOperation(s.disop)
@@ -88,7 +84,7 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
         c:RegisterEffect(e1)
     end
     
-    -- Effect 2: Banish Trap
+    -- Effect 2: Banish Trap (Thêm Once Per Turn)
     if (flag&0x2)~=0 then
         local e2=Effect.CreateEffect(c)
         e2:SetDescription(aux.Stringid(id,1))
@@ -96,6 +92,7 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
         e2:SetType(EFFECT_TYPE_QUICK_O)
         e2:SetCode(EVENT_CHAINING)
         e2:SetRange(LOCATION_MZONE)
+        e2:SetCountLimit(1)
         e2:SetCondition(s.rmvcon)
         e2:SetTarget(s.rmvtg)
         e2:SetOperation(s.rmvop)
@@ -103,14 +100,17 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
         c:RegisterEffect(e2)
     end
 
-    -- Effect 3: Draw 2
+    -- Effect 3: Draw 2 (Nếu muốn Draw ngay khi Summon chính mình)
     if (flag&0x4)~=0 then
+        Duel.Hint(HINT_CARD,0,id)
+        Duel.Draw(tp,2,REASON_EFFECT)
+        
+        -- Nếu vẫn muốn Draw khi các quái khác summon sau đó:
         local e3=Effect.CreateEffect(c)
-        e3:SetDescription(aux.Stringid(id,2))
-        e3:SetCategory(CATEGORY_DRAW)
         e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
         e3:SetRange(LOCATION_MZONE)
         e3:SetCode(EVENT_SUMMON_SUCCESS)
+        e3:SetProperty(EFFECT_FLAG_DELAY)
         e3:SetCondition(s.drcon)
         e3:SetTarget(s.drtg)
         e3:SetOperation(s.drop)
@@ -118,36 +118,34 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
         c:RegisterEffect(e3)
     end
 
-   -- Effect 4: Buff ATK & Immune
+    -- Effect 4: Buff ATK & Immune
     if (flag&0x8)~=0 then
-        -- Tăng ATK
         local e4a=Effect.CreateEffect(c)
         e4a:SetType(EFFECT_TYPE_SINGLE)
-        e4a:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
-        e4a:SetRange(LOCATION_MZONE) -- Phải có Range cho SINGLE_RANGE
+        e4a:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+        e4a:SetRange(LOCATION_MZONE)
         e4a:SetCode(EFFECT_UPDATE_ATTACK)
         e4a:SetValue(1000)
-        e4a:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+        e4a:SetReset(RESET_EVENT+RESETS_STANDARD)
         c:RegisterEffect(e4a)
 
-        -- Kháng hiệu ứng
         local e4b=Effect.CreateEffect(c)
         e4b:SetType(EFFECT_TYPE_SINGLE)
-        e4b:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+        e4b:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
         e4b:SetRange(LOCATION_MZONE)
         e4b:SetCode(EFFECT_IMMUNE_EFFECT)
         e4b:SetValue(s.efilter)
-        e4b:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+        e4b:SetReset(RESET_EVENT+RESETS_STANDARD)
         c:RegisterEffect(e4b)
     end
 end
 
--- Filter Kháng hiệu ứng
+-- Filter Kháng hiệu ứng đối thủ
 function s.efilter(e,re)
     return e:GetHandlerPlayer()~=re:GetOwnerPlayer()
 end
 
--- Negate Spell logic
+-- Negate Spell
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
     return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
         and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and Duel.IsChainNegatable(ev)
@@ -160,19 +158,18 @@ function s.disop(e,tp,eg,ep,ev,re,r,rp)
     Duel.NegateEffect(ev)
 end
 
--- Banish logic
+-- Banish logic (Non-targeting)
 function s.rmvcon(e,tp,eg,ep,ev,re,r,rp)
     return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
         and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_TRAP)
 end
 function s.rmvtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-    if chk==0 then return #g>0 end
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD)
 end
 function s.rmvop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
     if #g>0 then
         Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
     end
@@ -180,7 +177,7 @@ end
 
 -- Draw logic
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(Card.IsType,1,nil,TYPE_MONSTER)
+    return eg:IsExists(Card.IsType,1,e:GetHandler(),TYPE_MONSTER)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
