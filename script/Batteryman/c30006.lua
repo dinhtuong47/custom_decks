9595336ca638ea2653bc9b57879df1d3f01dddd6
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-05-06 02:10:04 +0700

    Update c30006.lua

--- script/Batteryman/c30006.lua
+++ script/Batteryman/c30006.lua
@@ -37,14 +37,15 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
     
     -- 4. Cấp hiệu ứng Bất tử: Dùng EFFECT_TYPE_GRANT để nó luôn hoạt động khi là nguyên liệu
-    -- Cách này hỗ trợ cả khi dùng Hazy Pillar nhét vào
-    local e4=Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
-    e4:SetRange(LOCATION_OVERLAY)
-    e4:SetTargetRange(LOCATION_MZONE,0)
-    e4:SetTarget(s.eftg)
-    e4:SetLabelObject(s.indes_effect())
-    c:RegisterEffect(e4)
+    --give effect
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_XMATERIAL)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e4:SetCondition(s.condition)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
 end
 
 -- Filter Link
@@ -81,15 +82,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 -- Hiệu ứng bất tử để cấp cho Xyz
-function s.indes_effect()
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    e1:SetValue(1)
-    return e1
-end
-
--- Target của EFFECT_GRANT: Chỉ cấp cho Basiltrice đang đè lên nó
-function s.eftg(e,c)
-    return e:GetHandler():GetOverlayTarget()==c and c:IsCode(23776077)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsCode(23776077)
 end
