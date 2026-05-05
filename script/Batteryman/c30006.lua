98ac11bb4a20a45cddb87e1a3c07099f92d0f206
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-05-06 01:33:13 +0700

    Update c30006.lua

--- script/Batteryman/c30006.lua
+++ script/Batteryman/c30006.lua
@@ -36,24 +36,23 @@ function s.initial_effect(c)
     e3:SetOperation(s.spop)
     c:RegisterEffect(e3)
     
+    -- Hiệu ứng được cấp: Không bị tiêu diệt bởi chiến đấu (Đưa vào trong s.initial_effect)
+    local e_res=Effect.CreateEffect(c)
+    e_res:SetType(EFFECT_TYPE_SINGLE)
+    e_res:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e_res:SetValue(1)
+    -- Reset khi nguyên liệu rời khỏi quái thú Xyz
+    e_res:SetReset(RESET_EVENT+RESETS_STANDARD)
+
     -- Cấp hiệu ứng cho quái thú Xyz (Basiltrice)
     local e4=Effect.CreateEffect(c)
     e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
     e4:SetRange(LOCATION_OVERLAY)
     e4:SetTargetRange(LOCATION_MZONE,0)
     e4:SetTarget(s.eftg)
-    e4:SetItemProperty(EFFECT_FLAG_CLIENT_HINT)
-    e4:SetLabelObject(s.eff_indes) -- Trỏ đến hiệu ứng muốn cấp
+    e4:SetLabelObject(e_res) -- Trỏ đến biến e_res vừa tạo ở trên
     c:RegisterEffect(e4)
 end
-
--- Hiệu ứng được cấp: Không bị tiêu diệt bởi chiến đấu
-s.eff_indes=Effect.CreateEffect(c)
-s.eff_indes:SetDescription(aux.Stringid(id,1))
-s.eff_indes:SetType(EFFECT_TYPE_SINGLE)
-s.eff_indes:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-s.eff_indes:SetValue(1)
-s.eff_indes:SetReset(RESET_EVENT+RESETS_STANDARD)
 
 -- Filter nguyên liệu Link
 function s.matfilter(c,scard,sumtype,tp)
@@ -74,11 +73,13 @@ end
 function s.spfilter(c,e,tp)
     return c:IsSetCard(0x67) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
+
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -88,7 +89,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- Target check: Chỉ cấp hiệu ứng nếu nó đang là Material của Basiltrice
+-- Target check: Chỉ cấp hiệu ứng nếu lá bài này đang nằm dưới Basiltrice
 function s.eftg(e,c)
     local g=e:GetHandler():GetOverlayTarget()
     return g and c==g and c:IsCode(23776077)
