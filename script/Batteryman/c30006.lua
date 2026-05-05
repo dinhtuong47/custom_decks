0fb27585d27e438529019e310dffacd805ce55e6
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-05-06 02:04:26 +0700

    Update c30006.lua

--- script/Batteryman/c30006.lua
+++ script/Batteryman/c30006.lua
@@ -4,16 +4,16 @@ function s.initial_effect(c)
     Link.AddProcedure(c,s.matfilter,1,1)
     c:EnableReviveLimit()
     
-    -- Ko bị chọn làm mục tiêu (Hazy Flame standard)
+    -- 1. Anti-Target
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
     e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
     e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
     e1:SetValue(aux.tgoval)
     c:RegisterEffect(e1)
     
-    -- FIX: Cấm Special Summon ngoại trừ Hazy Flame
+    -- 2. Cấm SS: Chỉ kích hoạt khi card đã lên sân thành công
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
     e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
@@ -23,55 +23,54 @@ function s.initial_effect(c)
     e2:SetTarget(s.splimit)
     c:RegisterEffect(e2)
     
-    -- FIX: Triệu hồi 1 Hazy Flame từ Deck khi Link Summon thành công
+    -- 3. Triệu hồi từ Deck: Thêm FLAG_DELAY để tránh bị lỡ timing
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,0))
     e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetProperty(EFFECT_FLAG_DELAY) 
     e3:SetCountLimit(1,id)
     e3:SetCondition(s.spcon)
     e3:SetTarget(s.sptg)
     e3:SetOperation(s.spop)
     c:RegisterEffect(e3)
     
-    -- FIX: Cấp hiệu ứng "Không bị tiêu diệt bởi chiến đấu" cho Basiltrice
+    -- 4. Cấp hiệu ứng Bất tử: Dùng EFFECT_TYPE_GRANT để nó luôn hoạt động khi là nguyên liệu
+    -- Cách này hỗ trợ cả khi dùng Hazy Pillar nhét vào
     local e4=Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-    e4:SetCode(EVENT_BE_MATERIAL)
-    e4:SetCondition(s.efcon)
-    e4:SetOperation(s.efop)
+    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+    e4:SetRange(LOCATION_OVERLAY)
+    e4:SetTargetRange(LOCATION_MZONE,0)
+    e4:SetTarget(s.eftg)
+    e4:SetLabelObject(s.indes_effect())
     c:RegisterEffect(e4)
 end
 
--- Filter nguyên liệu
+-- Filter Link
 function s.matfilter(c,scard,sumtype,tp)
     return c:IsLevel(6) and c:IsAttribute(ATTRIBUTE_FIRE)
 end
 
--- FIX logic cấm xồ: Trả về true nếu quái KHÔNG PHẢI Hazy Flame (để hệ thống cấm)
+-- Fix Cấm SS: Chỉ cấm nếu quái không phải Hazy
 function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
     return not c:IsSetCard(0x67)
 end
 
--- Điều kiện: Phải là Link Summon
+-- Check Link Summon
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
 
--- FIX Target SS: Kiểm tra Deck có quái Hazy không
+-- Target & Operation cho hiệu ứng xồ quái
 function s.spfilter(c,e,tp)
     return c:IsSetCard(0x67) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-
--- FIX Operation SS: Thực hiện gọi bài
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -81,19 +80,16 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
--- FIX Grant Effect: Sử dụng EVENT_BE_MATERIAL để gắn hiệu ứng vĩnh viễn vào quái Xyz
-function s.efcon(e,tp,eg,ep,ev,re,r,rp)
-    -- Kiểm tra xem có phải làm nguyên liệu cho Xyz Summon Basiltrice không
-    return r==REASON_XYZ and e:GetHandler():GetReasonCard():IsCode(23776077)
-end
-
-function s.efop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local rc=c:GetReasonCard() -- Basiltrice
+-- Hiệu ứng bất tử để cấp cho Xyz
+function s.indes_effect()
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
     e1:SetValue(1)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-    rc:RegisterEffect(e1)
+    return e1
+end
+
+-- Target của EFFECT_GRANT: Chỉ cấp cho Basiltrice đang đè lên nó
+function s.eftg(e,c)
+    return e:GetHandler():GetOverlayTarget()==c and c:IsCode(23776077)
 end
