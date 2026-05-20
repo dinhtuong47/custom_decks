325acf5d266ca29ffe7dac033c338a75c9edd5c8
dinhtuong47 <67795579+dinhtuong47@users.noreply.github.com>
2026-05-20 16:32:37 +0700

    Update c30000.lua

--- script/Batteryman/c30000.lua
+++ script/Batteryman/c30000.lua
@@ -9,18 +9,6 @@ function s.initial_effect(c)
     e1:SetTarget(s.eattg)
     e1:SetOperation(s.eatop)
     c:RegisterEffect(e1)
-
-    -- Hiệu ứng 2: Trong Battle Phase, tự xé bụng chui ra sân
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetRange(LOCATION_OVERLAY)
-    e2:SetCondition(s.sscon)
-    e2:SetTarget(s.sstg)
-    e2:SetOperation(s.ssop)
-    c:RegisterEffect(e2)
 end
 
 -- Logic chui vào bụng
@@ -35,27 +23,43 @@ function s.eatop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
         Duel.Overlay(tc,c)
+        
+        -- GẮN HIỆU ỨNG LÊN CON CHỦ ĐỂ HIỂN THỊ NÚT BẤM
+        local e2=Effect.CreateEffect(c)
+        e2:SetDescription(aux.Stringid(id,1)) -- Hiện đúng mô tả của con kí sinh
+        e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+        e2:SetType(EFFECT_TYPE_QUICK_O)
+        e2:SetCode(EVENT_FREE_CHAIN)
+        e2:SetRange(LOCATION_MZONE)
+        e2:SetLabelObject(c) -- Khóa mục tiêu vào đúng con c đang nằm dưới
+        e2:SetCondition(s.sscon)
+        e2:SetTarget(s.sstg)
+        e2:SetOperation(s.ssop)
+        e2:SetReset(RESET_EVENT+RESETS_STANDARD) -- Mất khi con chủ rời sân
+        tc:RegisterEffect(e2)
     end
 end
 
--- Logic xé bụng chui ra
+-- Logic xé bụng (Bấm trên con chủ, lấy con dưới ra)
 function s.sscon(e,tp,eg,ep,ev,re,r,rp)
     return Duel.IsBattlePhase()
 end
 
 function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
+    local c=e:GetLabelObject()
     if chk==0 then
-        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        return c and c:IsLocation(LOCATION_OVERLAY)
+            and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
             and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
     end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_OVERLAY)
 end
 
 function s.ssop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    -- Đã sửa: Check vị trí vật lý thay vì check liên kết hiệu ứng
-    if c:IsLocation(LOCATION_OVERLAY) then
-        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+    local c=e:GetLabelObject()
+    if c and c:IsLocation(LOCATION_OVERLAY) then
+        if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+            e:Reset() -- Giải phóng hiệu ứng trên con chủ sau khi đẻ xong
+        end
     end
 end
